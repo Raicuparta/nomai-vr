9eee32e35493b587535d0177535f0852256ed418
Ricardo Lopes <raicuparta@gmail.com>
2020-01-06 00:38:26 +0100

    Initial commit

---
+++ NomaiVR/NomaiVR.cs
@@ -0,0 +1,20 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using OWML.ModHelper.Events;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace OWML.NomaiVR
+{
+    public class NomaiVR : ModBehaviour
+    {
+        private void Start() {
+        }
+
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.P)) {
+            }
+        }
+
+    }
+}
