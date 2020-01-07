014357dfda73bb9277a8200dee99e54fb3af60e3
Ricardo Lopes <raicuparta@gmail.com>
2020-01-07 23:47:45 +0100

    Show dialogue canvas in VR

--- NomaiVR/Modules/Menus.cs
+++ NomaiVR/Modules/Menus.cs
@@ -5,6 +5,25 @@ namespace OWML.NomaiVR
 {
     public class Menus : MonoBehaviour
     {
+        protected class CanvasInfo {
+            public string name;
+            public Vector3 offset;
+
+            public CanvasInfo(string _name, Vector3 _offset) {
+                name = _name;
+                offset = _offset;
+            }
+            public CanvasInfo(string _name) {
+                name = _name;
+                offset = new Vector3(0, 0, 1);
+            }
+        }
+
+        static readonly CanvasInfo[] _canvasInfos = {
+            new CanvasInfo("PauseMenu", new Vector3(-0.6f, -0.5f, 1)),
+            new CanvasInfo("DialogueCanvas"),
+        };
+
         void Start() {
             NomaiVR.Log("Start Menus");
 
@@ -34,19 +53,26 @@ namespace OWML.NomaiVR
         }
 
         void MovePauseMenuToWorldSpace() {
-            GameObject pauseMenu = GameObject.Find("PauseMenu");
-            NomaiVR.Helper.Console.WriteLine("pauseMenu: " + pauseMenu.name);
-            Canvas[] canvases = pauseMenu.GetComponentsInChildren<Canvas>();
-            Camera playerCamera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
+            foreach (CanvasInfo canvasInfo in _canvasInfos) {
+                GameObject canvas = GameObject.Find(canvasInfo.name);
 
-            foreach (Canvas canvas in canvases) {
-                canvas.renderMode = RenderMode.WorldSpace;
+                if (canvas == null) {
+                    NomaiVR.Log("Couldn't find canvas with name: " + canvasInfo.name);
+                    continue;
+                }
+
+                Canvas[] subCanvases = canvas.GetComponentsInChildren<Canvas>();
+                Camera playerCamera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
+
+                foreach (Canvas subCanvas in subCanvases) {
+                    subCanvas.renderMode = RenderMode.WorldSpace;
+                }
+
+                canvas.transform.parent = playerCamera.transform;
+                canvas.transform.localPosition = canvasInfo.offset;
+                canvas.transform.localEulerAngles = new Vector3(0, 0, 0);
+                canvas.transform.localScale = Vector3.one * 0.001f;
             }
-
-            pauseMenu.transform.parent = playerCamera.transform;
-            pauseMenu.transform.localPosition = new Vector3(-0.6f, -0.5f, 1);
-            pauseMenu.transform.localEulerAngles = new Vector3(0, 0, 0);
-            pauseMenu.transform.localScale = Vector3.one * 0.001f;
         }
 
     }
