2b2d2cea432e7464459b8313aaab743df533dcf8
Ricardo Lopes <Raicuparta@users.noreply.github.com>
2020-01-07 13:07:42 +0100

    Player body following camera position (#15)
    * WIP: Player movement following headset
    
    * WIP: use head position instead of body position
    
    * WIP: fixed movement rate
    
    * WIP: Allow vertical camera movement
    
    * Cleanup

--- NomaiVR/NomaiVR.cs
+++ NomaiVR/NomaiVR.cs
@@ -3,14 +3,56 @@ using OWML.ModHelper;
 using OWML.ModHelper.Events;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.XR;
 
 namespace OWML.NomaiVR
 {
     public class NomaiVR : ModBehaviour
     {
+        Rigidbody _playerBody;
+        Camera _mainCamera;
+        GameObject _cameraParent;
+        Transform _playerHead;
+        bool _isAwake;
+        Vector3 _prevCameraPosition;
+
         void Start() {
             SceneManager.sceneLoaded += OnSceneLoaded;
-            MoveMainMenuToWorldSpace();
+            MoveAllCanvasesToWorldSpace();
+
+            ModHelper.Events.Subscribe<Flashlight>(Events.AfterStart);
+            ModHelper.Events.OnEvent += OnWakeUp;
+        }
+
+        private void updateMainCamera() {
+            _mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+            ModHelper.Console.WriteLine("Main camera: " + _mainCamera.name);
+
+            //Make an empty parent object for moving the camera around.
+            _cameraParent = new GameObject();
+            _cameraParent.transform.parent = _mainCamera.transform.parent;
+            _cameraParent.transform.localPosition = Vector3.zero;
+            _cameraParent.transform.localRotation = Quaternion.identity;
+            _mainCamera.transform.parent = _cameraParent.transform;
+
+            // This component is messing with our ability to read the VR camera's rotation.
+            // I'm disabling it even though I have no clue what it does ¯\_(ツ)_/¯
+            PlayerCameraController playerCameraController = _mainCamera.GetComponent<PlayerCameraController>();
+            if (playerCameraController) {
+                playerCameraController.enabled = false;
+            }
+        }
+
+        private void OnWakeUp(MonoBehaviour behaviour, Events ev) {
+            _isAwake = true;
+
+            _playerBody = GameObject.Find("Player_Body").GetComponent<Rigidbody>();
+            _playerHead = FindObjectOfType<ToolModeUI>().transform;
+
+            // Set initial camera position to player head.
+            Vector3 movement = _playerHead.position - _mainCamera.transform.position;
+            _cameraParent.transform.position += movement;
         }
 
         void OnDisable() {
@@ -19,9 +61,10 @@ namespace OWML.NomaiVR
 
         void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
             MovePauseMenuToWorldSpace();
+            updateMainCamera();
         }
 
-        void MoveMainMenuToWorldSpace() {
+        void MoveAllCanvasesToWorldSpace() {
             Canvas[] canvases = Object.FindObjectsOfType<Canvas>();
 
             foreach (Canvas canvas in canvases) {
@@ -36,6 +79,7 @@ namespace OWML.NomaiVR
 
         void MovePauseMenuToWorldSpace() {
             GameObject pauseMenu = GameObject.Find("PauseMenu");
+            ModHelper.Console.WriteLine("pauseMenu: " + pauseMenu.name);
             Canvas[] canvases = pauseMenu.GetComponentsInChildren<Canvas>();
             Camera playerCamera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
 
@@ -47,6 +91,25 @@ namespace OWML.NomaiVR
             pauseMenu.transform.localPosition = new Vector3(-0.6f, -0.5f, 1);
             pauseMenu.transform.localEulerAngles = new Vector3(0, 0, 0);
             pauseMenu.transform.localScale = Vector3.one * 0.001f;
+        }
+
+        void MovePlayerBodyToCamera() {
+            // Move player to camera position.
+            Vector3 movement = _prevCameraPosition - (_playerHead.position - _mainCamera.transform.position);
+            _playerBody.transform.localPosition += movement;
+
+            // Since camera is a child of player body, we need to adjust it now.
+            Vector3 cameraMovement = _cameraParent.transform.InverseTransformVector(movement);
+            cameraMovement.y = 0;
+            _cameraParent.transform.localPosition -= cameraMovement;
+
+            _prevCameraPosition = _playerHead.position - _mainCamera.transform.position;
+        }
+
+        void Update() {
+            if (_isAwake) {
+                MovePlayerBodyToCamera();
+            }
         }
 
     }
