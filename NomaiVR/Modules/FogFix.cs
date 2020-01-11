c859a77350232e1eebc90daba1966242a69019c8
Ricardo Lopes <raicuparta@gmail.com>
2020-01-11 17:46:09 +0100

    Just disable Fog resetting for now

--- NomaiVR/Modules/FogFix.cs
+++ NomaiVR/Modules/FogFix.cs
@@ -9,9 +9,14 @@ namespace Raicuparta.NomaiVR
     public class FogFix : MonoBehaviour
     {
         private void Start() {
-            NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("ResetFogSettings", typeof(Patches), "PatchResetFog");
-            NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("UpdateFogSettings", typeof(Patches), "PatchUpdateFog");
-            NomaiVR.Helper.HarmonyHelper.AddPrefix<FogOverrideVolume>("OverrideFogSettings", typeof(Patches), "PatchOverrideFog");
+            // This might almost work, but then the other cameras (like the probe) will affect the counts.
+            // So I'm disabling for now.
+            //NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("ResetFogSettings", typeof(Patches), "PatchResetFog");
+            //NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("UpdateFogSettings", typeof(Patches), "PatchUpdateFog");
+            //NomaiVR.Helper.HarmonyHelper.AddPrefix<FogOverrideVolume>("OverrideFogSettings", typeof(Patches), "PatchOverrideFog");
+
+            // This just prevents the fog from resetting. Looks fine I think?
+            NomaiVR.Helper.HarmonyHelper.EmptyMethod<PlanetaryFogController>("ResetFogSettings");
         }
     }
 
