077524f49379143975c284a7ebcbcf6965e9ccf7
Ricardo Lopes <raicuparta@gmail.com>
2020-01-14 02:59:15 +0100

    Holding signalscope very well

--- NomaiVR/Modules/MotionControls.cs
+++ NomaiVR/Modules/MotionControls.cs
@@ -8,6 +8,7 @@ namespace NomaiVR
     public class MotionControls : MonoBehaviour
     {
         Transform _hand;
+        Transform _handParent;
         void Start() {
             NomaiVR.Log("Start MotionControls");
 
@@ -25,23 +26,19 @@ namespace NomaiVR
         }
 
         void SetupMotion() {
-
-
             var rightHand = Instantiate(GameObject.Find("PlayerSuit_Glove_Right"));
             var handParent = new GameObject();
+            _handParent = handParent.transform;
             _hand = rightHand.transform;
             _hand.parent = handParent.transform;
             _hand.localRotation = Quaternion.Euler(45, 180, 0);
             _hand.localPosition = new Vector3(0, -0.03f, -0.08f);
             _hand.localScale = Vector3.one * 0.5f;
 
-            setup(handParent);
-
+            addPoseDriver(handParent);
 
             var signalScope = GameObject.Find("Signalscope");
-            signalScope.transform.parent = handParent.transform;
-            signalScope.transform.localPosition = Vector3.zero;
-            signalScope.transform.localRotation = Quaternion.identity;
+            HoldObject(signalScope, new Vector3(-0.035f, 0.017f, 0.106f), Quaternion.Euler(32, 0, 0));
             ////var probeLauncher = GameObject.Find("ProbeLauncher");
             ////setup(signalScope);
             ////setup(probeLauncher);
@@ -51,16 +48,13 @@ namespace NomaiVR
             // and screws with perspective. Changing to Standard shader so they look
             // like a normal 3D object.
             signalScopeModel.GetComponent<MeshRenderer>().material.shader = Shader.Find("Standard");
-            signalScopeModel.localPosition = Vector3.up * -0.1f;
+            signalScopeModel.localPosition = Vector3.zero;
             signalScopeModel.localRotation = Quaternion.identity;
 
             // This child seems to be only for some kind of shader effect.
             // Disabling it since it doesn't seem necessary.
             signalScopeModel.GetChild(0).gameObject.SetActive(false);
 
-            ////addPoseDriver(probeLauncher);
-            ////addPoseDriver(signalScope);
-            addPoseDriver(handParent);
 
             // Attatch Signalscope UI to the Signalscope.
             var reticule = GameObject.Find("SignalscopeReticule").GetComponent<Canvas>();
@@ -71,49 +65,33 @@ namespace NomaiVR
             reticule.transform.localRotation = Quaternion.identity;
         }
 
-        void setup(GameObject gameObject) {
-            gameObject.transform.parent = Common.MainCamera.transform.parent;
-            gameObject.transform.position = Common.MainCamera.transform.position;
+        void HoldObject(GameObject gameObject, Vector3 position, Quaternion rotation) {
+            var objectParent = new GameObject().transform;
+            objectParent.parent = _handParent;
+            objectParent.localPosition = position;
+            objectParent.localRotation = rotation;
+            gameObject.transform.parent = objectParent;
+            gameObject.transform.localPosition = Vector3.zero;
             gameObject.transform.localRotation = Quaternion.identity;
         }
 
+        void HoldObject(GameObject gameObject) {
+            HoldObject(gameObject, Vector3.zero, Quaternion.identity);
+        }
+        void HoldObject(GameObject gameObject, Quaternion rotation) {
+            HoldObject(gameObject, Vector3.zero, rotation);
+        }
+        void HoldObject(GameObject gameObject, Vector3 position) {
+            HoldObject(gameObject, position, Quaternion.identity);
+        }
+
         void addPoseDriver(GameObject gameObject) {
+            gameObject.transform.parent = Common.MainCamera.transform.parent;
+            gameObject.transform.position = Common.MainCamera.transform.position;
+            gameObject.transform.localRotation = Quaternion.identity;
             var poseDriver = gameObject.AddComponent<TrackedPoseDriver>();
             poseDriver.SetPoseSource(TrackedPoseDriver.DeviceType.GenericXRController, TrackedPoseDriver.TrackedPose.RightPose);
             //poseDriver.UseRelativeTransform = true;
         }
-
-        void Update() {
-            if (_hand) {
-                var amount = 0.01f;
-                Vector3 handPosition = _hand.localPosition;
-                if (Input.GetKeyDown(KeyCode.Keypad7)) {
-                    handPosition.x += amount;
-                }
-                if (Input.GetKeyDown(KeyCode.Keypad4)) {
-                    handPosition.x -= amount;
-                }
-                if (Input.GetKeyDown(KeyCode.Keypad8)) {
-                    handPosition.y += amount;
-                }
-                if (Input.GetKeyDown(KeyCode.Keypad5)) {
-                    handPosition.y -= amount;
-                }
-                if (Input.GetKeyDown(KeyCode.Keypad9)) {
-                    handPosition.z += amount;
-                }
-                if (Input.GetKeyDown(KeyCode.Keypad6)) {
-                    handPosition.z -= amount;
-                }
-
-                if (Input.anyKeyDown) {
-                    NomaiVR.Log("x: " + handPosition.x);
-                    NomaiVR.Log("y: " + handPosition.y);
-                    NomaiVR.Log("z: " + handPosition.z);
-                    _hand.localPosition = handPosition;
-                }
-            }
-        }
-
     }
 }
