e7790fc6411faa012b53a8f3277f9b8e393ac5dc
Ricardo Lopes <raicuparta@gmail.com>
2020-01-17 20:50:14 +0100

    Cleanup unnecessary shit from SteamVR

--- NomaiVR/Modules/SteamVRTest.cs
+++ NomaiVR/Modules/SteamVRTest.cs
@@ -12,16 +12,18 @@ namespace NomaiVR
 
             var hand = new GameObject();
 
-            var pose = hand.AddComponent<SteamVR_Behaviour_Pose>();
-            pose.poseAction = SteamVR_Input.GetAction<SteamVR_Action_Pose>("Pose");
-            pose.inputSource = SteamVR_Input_Sources.RightHand;
+            SteamVR.Initialize();
 
-            var interaction = hand.AddComponent<Valve.VR.InteractionSystem.Hand>();
-            interaction.handType = SteamVR_Input_Sources.RightHand;
-            interaction.grabPinchAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabPinch");
-            interaction.grabGripAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
-            interaction.hapticAction = SteamVR_Input.GetAction<SteamVR_Action_Vibration>("Haptic");
-            interaction.uiInteractAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
+            //var pose = hand.AddComponent<SteamVR_Behaviour_Pose>();
+            //pose.poseAction = SteamVR_Input.GetAction<SteamVR_Action_Pose>("Pose");
+            //pose.inputSource = SteamVR_Input_Sources.RightHand;
+
+            //var interaction = hand.AddComponent<Valve.VR.InteractionSystem.Hand>();
+            //interaction.handType = SteamVR_Input_Sources.RightHand;
+            //interaction.grabPinchAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabPinch");
+            //interaction.grabGripAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
+            //interaction.hapticAction = SteamVR_Input.GetAction<SteamVR_Action_Vibration>("Haptic");
+            //interaction.uiInteractAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
 
             var grabGrip = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
             grabGrip.onChange += GrabGrip_onChange;
@@ -35,7 +37,7 @@ namespace NomaiVR
         }
 
         private void GrabGrip_onChange(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool newState) {
-            NomaiVR.Log("YO MR WHITwE");
+            NomaiVR.Log("YO MR WHIaaTwE " + fromAction.localizedOriginName);
         }
     }
 }
