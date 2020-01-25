0b49e8bf9b800d4600628f9a5f172af1aaa41cee
Ricardo Lopes <Raicuparta@users.noreply.github.com>
2020-01-25 22:32:12 +0100

    Disable fog events on certain eyes (#54)
    * Disable fog events on certain eyes
    
    * Remove debug stuff

--- NomaiVR/Modules/FogFix.cs
+++ NomaiVR/Modules/FogFix.cs
@@ -5,30 +5,24 @@ namespace NomaiVR
     public class FogFix : MonoBehaviour
     {
         private void Start() {
-            // This might almost work, but then the other cameras (like the probe) will affect the counts.
-            // So I'm disabling for now.
-            //NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("ResetFogSettings", typeof(Patches), "PatchResetFog");
-            //NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("UpdateFogSettings", typeof(Patches), "PatchUpdateFog");
-            //NomaiVR.Helper.HarmonyHelper.AddPrefix<FogOverrideVolume>("OverrideFogSettings", typeof(Patches), "PatchOverrideFog");
+            NomaiVR.Log("Started FogFix");
 
-            // This just prevents the fog from resetting. Looks fine I think?
-            NomaiVR.Helper.HarmonyHelper.EmptyMethod<PlanetaryFogController>("ResetFogSettings");
+            NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("ResetFogSettings", typeof(Patches), "PatchResetFog");
+            NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("UpdateFogSettings", typeof(Patches), "PatchUpdateFog");
+            NomaiVR.Helper.HarmonyHelper.AddPrefix<FogOverrideVolume>("OverrideFogSettings", typeof(Patches), "PatchOverrideFog");
         }
-    }
 
-    internal static class Patches
-    {
-        static bool _isEvenFrame = true;
-
-        static bool PatchResetFog() {
-            _isEvenFrame = !_isEvenFrame;
-            return _isEvenFrame;
-        }
-        static bool PatchUpdateFog() {
-            return _isEvenFrame;
-        }
-        static bool PatchOverrideFog() {
-            return !_isEvenFrame;
+        internal static class Patches
+        {
+            static bool PatchResetFog() {
+                return Camera.current.stereoActiveEye != Camera.MonoOrStereoscopicEye.Left;
+            }
+            static bool PatchUpdateFog() {
+                return Camera.current.stereoActiveEye != Camera.MonoOrStereoscopicEye.Right;
+            }
+            static bool PatchOverrideFog() {
+                return Camera.current.stereoActiveEye != Camera.MonoOrStereoscopicEye.Right;
+            }
         }
     }
 
