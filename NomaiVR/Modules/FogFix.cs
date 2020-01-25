5259d084364bd4923b0c9088663efabfe44c340e
Ricardo Lopes <raicuparta@gmail.com>
2020-01-25 22:24:07 +0100

    Disable fog events on certain eyes

--- NomaiVR/Modules/FogFix.cs
+++ NomaiVR/Modules/FogFix.cs
@@ -4,31 +4,47 @@ namespace NomaiVR
 {
     public class FogFix : MonoBehaviour
     {
+        static protected bool resetFog = false;
+        static protected bool updateFog = false;
+        static protected bool overrideFog = false;
+
         private void Start() {
             // This might almost work, but then the other cameras (like the probe) will affect the counts.
             // So I'm disabling for now.
-            //NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("ResetFogSettings", typeof(Patches), "PatchResetFog");
-            //NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("UpdateFogSettings", typeof(Patches), "PatchUpdateFog");
-            //NomaiVR.Helper.HarmonyHelper.AddPrefix<FogOverrideVolume>("OverrideFogSettings", typeof(Patches), "PatchOverrideFog");
+            NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("ResetFogSettings", typeof(Patches), "PatchResetFog");
+            NomaiVR.Helper.HarmonyHelper.AddPrefix<PlanetaryFogController>("UpdateFogSettings", typeof(Patches), "PatchUpdateFog");
+            NomaiVR.Helper.HarmonyHelper.AddPrefix<FogOverrideVolume>("OverrideFogSettings", typeof(Patches), "PatchOverrideFog");
 
             // This just prevents the fog from resetting. Looks fine I think?
-            NomaiVR.Helper.HarmonyHelper.EmptyMethod<PlanetaryFogController>("ResetFogSettings");
+            //NomaiVR.Helper.HarmonyHelper.EmptyMethod<PlanetaryFogController>("ResetFogSettings");
         }
-    }
 
-    internal static class Patches
-    {
-        static bool _isEvenFrame = true;
+        void Update () {
+            if (Input.GetKeyDown(KeyCode.J)) {
+                resetFog = !resetFog;
+            }
+            if (Input.GetKeyDown(KeyCode.K)) {
+                updateFog = !updateFog;
+            }
+            if (Input.GetKeyDown(KeyCode.L)) {
+                overrideFog = !overrideFog;
+            }
+            if (Input.anyKeyDown) {
+                NomaiVR.Log("reset " + resetFog + ", update " + updateFog + ", override " + overrideFog);
+            }
+        }
 
-        static bool PatchResetFog() {
-            _isEvenFrame = !_isEvenFrame;
-            return _isEvenFrame;
-        }
-        static bool PatchUpdateFog() {
-            return _isEvenFrame;
-        }
-        static bool PatchOverrideFog() {
-            return !_isEvenFrame;
+        internal static class Patches
+        {
+            static bool PatchResetFog() {
+                return Camera.current.stereoActiveEye != Camera.MonoOrStereoscopicEye.Left;
+            }
+            static bool PatchUpdateFog() {
+                return Camera.current.stereoActiveEye != Camera.MonoOrStereoscopicEye.Right;
+            }
+            static bool PatchOverrideFog() {
+                return Camera.current.stereoActiveEye != Camera.MonoOrStereoscopicEye.Right;
+            }
         }
     }
 
