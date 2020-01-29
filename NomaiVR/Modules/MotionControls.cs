aeac31eb9fccc6b8e588c04305c7f8caf3796a01
Ricardo Lopes <raicuparta@gmail.com>
2020-01-29 19:21:14 +0100

    Refactor hold translator

--- NomaiVR/Modules/MotionControls.cs
+++ NomaiVR/Modules/MotionControls.cs
@@ -63,9 +63,9 @@ namespace NomaiVR
 
                 HideArms();
                 HoldProbeLauncher();
-                HoldTranslator();
                 HoldHUD();
                 gameObject.AddComponent<HoldMallowStick>();
+                gameObject.AddComponent<HoldTranslator>();
                 gameObject.AddComponent<HoldSignalscope>();
 
                 // Move helmet forward to make it a bit more visible.
@@ -184,50 +184,6 @@ namespace NomaiVR
             displayImage.localRotation = Quaternion.identity;
 
             playerHUD.Find("HelmetOnUI/UICanvas/HUDProbeDisplay/Image").gameObject.SetActive(false);
-        }
-
-        void HoldTranslator() {
-            var translator = Common.MainCamera.transform.Find("NomaiTranslatorProp");
-
-            HoldObject(translator, RightHand, new Vector3(-0.24f, 0.08f, 0.06f), Quaternion.Euler(32.8f, 0f, 0f));
-
-            var translatorGroup = translator.Find("TranslatorGroup");
-            translatorGroup.localPosition = Vector3.zero;
-            translatorGroup.localRotation = Quaternion.identity;
-
-            translator.localScale = Vector3.one * 0.3f;
-            var translatorModel = translatorGroup.Find("Props_HEA_Translator");
-            translatorModel.localPosition = Vector3.zero;
-            translatorModel.localRotation = Quaternion.identity;
-
-            translator.GetComponent<NomaiTranslator>().SetValue("_raycastTransform", translatorModel);
-
-            // This child seems to be only for some kind of shader effect.
-            // Disabling it since it looks glitchy and doesn't seem necessary.
-            translatorModel.Find("Props_HEA_Translator_Prepass").gameObject.SetActive(false);
-
-            var renderers = translatorModel.gameObject.GetComponentsInChildren<MeshRenderer>(true);
-
-            foreach (var renderer in renderers) {
-                foreach (var material in renderer.materials) {
-                    material.shader = Shader.Find("Standard");
-                }
-            }
-
-            var texts = translator.gameObject.GetComponentsInChildren<Graphic>(true);
-
-            foreach (var text in texts) {
-                text.material = null;
-            }
-
-            var translatorHolster = Instantiate(translatorModel).gameObject;
-            translatorHolster.SetActive(true);
-            var holster = translatorHolster.AddComponent<HolsterTool>();
-            holster.hand = RightHand;
-            holster.position = new Vector3(-0.3f, 0.35f, 0);
-            holster.mode = ToolMode.Translator;
-            holster.scale = 0.15f;
-            holster.angle = new Vector3(0, 90, 90);
         }
 
         public static void HoldObject(Transform objectTransform, Transform hand, Vector3 position, Quaternion rotation) {
