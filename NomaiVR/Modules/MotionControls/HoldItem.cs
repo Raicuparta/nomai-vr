72ff0d19d802fb3893fc298d9782bda9519aa381
Ricardo Lopes <raicuparta@gmail.com>
2020-02-01 15:09:22 +0100

    Adjust holdable item position

--- NomaiVR/Modules/MotionControls/HoldItem.cs
+++ NomaiVR/Modules/MotionControls/HoldItem.cs
@@ -4,7 +4,8 @@ namespace NomaiVR {
     class HoldItem: MonoBehaviour {
         void Awake () {
             var itemCarryTool = GameObject.FindObjectOfType<ItemTool>();
-            Hands.HoldObject(itemCarryTool.transform, Hands.RightHand);
+            itemCarryTool.transform.localScale = 1.8f * Vector3.one;
+            Hands.HoldObject(itemCarryTool.transform, Hands.RightHand, new Vector3(-0.17f, 0.08f, -0.54f), Quaternion.Euler(340, 16.41602f, 280));
         }
     }
 }
