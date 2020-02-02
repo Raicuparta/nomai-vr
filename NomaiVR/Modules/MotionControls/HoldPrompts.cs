b94f9f51c03bc3cec108f22200e91cd4cd3a9706
Ricardo Lopes <raicuparta@gmail.com>
2020-02-02 02:42:07 +0100

    Prompts facing camera correctly

--- NomaiVR/Modules/MotionControls/HoldPrompts.cs
+++ NomaiVR/Modules/MotionControls/HoldPrompts.cs
@@ -3,9 +3,12 @@
 namespace NomaiVR {
     public class HoldPrompts: MonoBehaviour {
         Transform _holdTransform;
+        int _debugChild = 0;
+        Transform _canvasTransform;
 
         void Awake () {
             var canvas = GameObject.Find("ScreenPromptCanvas").GetComponent<Canvas>();
+            _canvasTransform = canvas.transform;
             canvas.transform.localScale = Vector3.one * 0.0015f;
             canvas.transform.localPosition = Vector3.zero;
             canvas.transform.localRotation = Quaternion.identity;
@@ -14,11 +17,26 @@ namespace NomaiVR {
             canvas.transform.localPosition = Vector3.zero;
             canvas.transform.localRotation = Quaternion.identity;
 
-            _holdTransform = Hands.HoldObject(canvas.transform, Hands.RightHand, new Vector3(0.21f, 0f, 0.12f));
+            _holdTransform = Hands.HoldObject(canvas.transform, Hands.RightHand, new Vector3(-0.09f, -0.11f, 0.13f));
+            _holdTransform.gameObject.AddComponent<DebugTransform>();
+
+            foreach (Transform child in canvas.transform) {
+                child.localPosition = Vector3.zero;
+            }
         }
 
         void Update () {
-            _holdTransform.LookAt(Common.MainCamera.transform.position + Common.MainCamera.transform.forward * 30, Common.PlayerHead.up);
+            _holdTransform.LookAt(2 * _holdTransform.position - Common.MainCamera.transform.position, Common.PlayerHead.up);
+
+            if (Input.GetKeyDown(KeyCode.Keypad3)) {
+                Destroy(_canvasTransform.GetChild(_debugChild).gameObject.GetComponent<DebugTransform>());
+                if (_debugChild < 5)
+                    _debugChild += 1;
+                else {
+                    _debugChild = 0;
+                }
+                _canvasTransform.GetChild(_debugChild).gameObject.AddComponent<DebugTransform>();
+            }
         }
     }
 }
