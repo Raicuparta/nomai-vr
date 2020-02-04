879647ee613b99f38b017aba6ee380cbd6b2fdb1
Ricardo Lopes <raicuparta@gmail.com>
2020-02-04 23:12:17 +0100

    Taking pictures by interacting with screen

--- NomaiVR/Modules/MotionControls/ShipTools.cs
+++ NomaiVR/Modules/MotionControls/ShipTools.cs
@@ -3,40 +3,40 @@ using UnityEngine;
 
 namespace NomaiVR {
     class ShipTools: MonoBehaviour {
+        static ProbeLauncher _shipProbeLauncher;
         void Awake () {
             NomaiVR.Log("Start Ship Tools");
 
+            _shipProbeLauncher = GameObject.FindObjectOfType<ShipCockpitController>().GetShipProbeLauncher();
+            _shipProbeLauncher.EquipTool();
             NomaiVR.Helper.HarmonyHelper.AddPostfix<ShipBody>("Start", typeof(Patches), "ShipStart");
         }
 
         internal static class Patches {
             static void ShipStart (ShipBody __instance) {
-                foreach (Transform child in __instance.transform) {
-                    NomaiVR.Log("Patch ship", child.gameObject.name);
-                }
                 var cockpitUI = __instance.transform.Find("Module_Cockpit/Systems_Cockpit/ShipCockpitUI");
-                NomaiVR.Log("cockpitUI ", cockpitUI.name);
-                var probeScreen = cockpitUI.Find("ProbeScreen").gameObject;
-                NomaiVR.Log("probeScreen ", probeScreen.name);
+                var probeScreen = cockpitUI.Find("ProbeScreen/ProbeScreenPivot/ProbeCamDisplay").gameObject;
                 var signalscopeScreen = cockpitUI.Find("SignalScreen").gameObject;
-                NomaiVR.Log("signalscopeScreen ", signalscopeScreen.name);
 
                 var collider = probeScreen.AddComponent<SphereCollider>();
-                collider.radius = 0.2f;
+                collider.radius = 0.4f;
                 collider.isTrigger = true;
 
                 var interaction = probeScreen.AddComponent<InteractReceiver>();
-                NomaiVR.Log("interaction ", interaction.name);
                 interaction.SetInteractRange(2);
-                NomaiVR.Log("_usableInShip before");
-
                 interaction.SetValue("_usableInShip", true);
-                NomaiVR.Log("_usableInShip after");
                 interaction.SetPromptText(UITextType.Probe_Title);
+                interaction.OnPressInteract += OnProbeInteract;
 
-                NomaiVR.Log("after interaction ");
+            }
 
-
+            private static void OnProbeInteract () {
+                var activeProbe = _shipProbeLauncher.GetActiveProbe();
+                if (activeProbe == null) {
+                    _shipProbeLauncher.Invoke("LaunchProbe");
+                } else {
+                    _shipProbeLauncher.Invoke("TakeSnapshotWithCamera", activeProbe.GetForwardCamera());
+                }
             }
         }
     }
