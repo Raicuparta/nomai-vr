2364316a7df883833882549c852788b0fbe22246
stephaje <57157278+stephaje@users.noreply.github.com>
2020-02-07 17:47:36 -0600

    Issue #99 Dialogue boxes rotate with the NPC, and so can appear backwards and behind them (#100)
    * Dialog now sticks to NPCs/attention points
    
    Dialog now sticks to the attention point of the dialog. The player can now turn their head to read the text more easily.
    
    * Update Menus.cs
    
    * csproj cleanup
    
    * Allow Dialog to move within a conversation
    
    Objects can sometimes move during a conversation. This change forces updates of the dialog positioning to continue rendering in the same spot.
    
    * Hands changes
    
    * Reverting experimenting
    
    * Removing unnecessary library

--- NomaiVR/Modules/Dialog.cs
+++ NomaiVR/Modules/Dialog.cs
@@ -1,32 +1,52 @@
-﻿using OWML.ModHelper.Events;
-using UnityEngine;
+﻿using UnityEngine;
+using OWML.ModHelper.Events;
 
 namespace NomaiVR {
     public class Dialog: MonoBehaviour {
+
+        private static CharacterDialogueTree currentDialogue;
+        private static GameObject dialogCanvas;
+        // distance away from the player the dialog renders
+        private static float dialogRenderDistance = 1f;
+        private static float dialogRenderSize = 0.001f;
 
         void Start () {
             NomaiVR.Log("Started Dialog Fixes");
 
             NomaiVR.Helper.HarmonyHelper.AddPrefix<CharacterDialogueTree>("StartConversation", typeof(Patches), "PatchStartConversation");
+            NomaiVR.Helper.HarmonyHelper.AddPrefix<CharacterDialogueTree>("EndConversation", typeof(Patches), "PatchEndConversation");
+
+            currentDialogue = null;
+            dialogCanvas = GameObject.Find(Menus.CanvasTypes.DialogueCanvas);
+            if (dialogCanvas == null) {
+                NomaiVR.Log("Couldn't find canvas with name: " + Menus.CanvasTypes.DialogueCanvas);
+            } else {
+                dialogCanvas.transform.localScale = Vector3.one * dialogRenderSize;
+            }
         }
 
-        internal static class Patches {
-            static void PatchStartConversation (CharacterDialogueTree __instance) {
-                GameObject dialogCanvas = GameObject.Find(Menus.CanvasTypes.DialogueCanvas);
-                if (dialogCanvas == null) {
-                    NomaiVR.Log("Couldn't find canvas with name: " + Menus.CanvasTypes.DialogueCanvas);
-                    return;
-                }
-
-                Transform attentionPoint = __instance.GetValue<Transform>("_attentionPoint");
+        void Update () {
+            if ((currentDialogue != null) && (dialogCanvas != null)) {
+                Transform attentionPoint = currentDialogue.GetValue<Transform>("_attentionPoint");
                 dialogCanvas.transform.parent = attentionPoint;
                 dialogCanvas.transform.localPosition = Vector3.zero;
                 dialogCanvas.transform.LookAt(2 * attentionPoint.position - Common.MainCamera.transform.position, Common.PlayerHead.up);
 
                 // Move so it is 1 unit away from the player
                 float distance = Vector3.Distance(attentionPoint.position, Common.MainCamera.transform.position);
-                dialogCanvas.transform.position = Vector3.MoveTowards(attentionPoint.position, Common.MainCamera.transform.position, distance - 1f);
+                dialogCanvas.transform.position = Vector3.MoveTowards(attentionPoint.position, Common.MainCamera.transform.position, distance - dialogRenderDistance);
+            }
+        }
+
+        internal static class Patches {
+            static void PatchStartConversation (CharacterDialogueTree __instance) {
+                currentDialogue = __instance;
+            }
+            static void PatchEndConversation () {
+                currentDialogue = null;
             }
         }
     }
+
+
 }
