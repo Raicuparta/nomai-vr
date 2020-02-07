1c7027384df51f0d96dd408d540a1aa35890c813
stephaje <57157278+stephaje@users.noreply.github.com>
2020-02-07 17:43:26 -0600

    Reverting experimenting

--- NomaiVR/Modules/MotionControls/Hands.cs
+++ NomaiVR/Modules/MotionControls/Hands.cs
@@ -111,7 +111,6 @@ namespace NomaiVR {
             objectParent.parent = hand;
             objectParent.localPosition = position;
             objectParent.localRotation = rotation;
-            objectParent.name = objectTransform.name + " parent";
             objectTransform.transform.parent = objectParent;
             objectTransform.transform.localPosition = Vector3.zero;
             objectTransform.transform.localRotation = Quaternion.identity;
@@ -139,16 +138,6 @@ namespace NomaiVR {
             if (_wrapper && Camera.main) {
                 _wrapper.localPosition = Camera.main.transform.localPosition - InputTracking.GetLocalPosition(XRNode.CenterEye);
             }
-
-            var thrusterHUD = GameObject.Find("HUD_Thrusters");
-
-            //var thrustHUD = GameObject.FindObjectOfType<ThrustAndAttitudeIndicator>();
-
-            thrusterHUD.transform.LookAt(Common.MainCamera.transform.position, Common.PlayerHead.up);
-            //thrusterHUD.transform.
-            
-
-            //dialogCanvas.transform.LookAt(2 * attentionPoint.position - Common.MainCamera.transform.position, Common.PlayerHead.up);
         }
     }
 }
