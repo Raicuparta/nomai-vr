466a56ad40e0c22bc362191ac2a1a966abd306c8
Ricardo Lopes <raicuparta@gmail.com>
2020-02-08 18:12:17 +0100

    Stop rotation completely if looking down

--- NomaiVR/Modules/PlayerBodyPosition.cs
+++ NomaiVR/Modules/PlayerBodyPosition.cs
@@ -13,13 +13,6 @@ namespace NomaiVR {
             updateMainCamera();
             NomaiVR.Helper.HarmonyHelper.AddPostfix<PlayerCharacterController>("UpdateTurning", typeof(Patches), "PatchTurning");
             MoveCameraToPlayerHead();
-
-            InvokeRepeating("LogMagnitude", 0.5f, 0.5f);
-        }
-
-        void LogMagnitude () {
-            var magnitude = Vector3.ProjectOnPlane(Camera.main.transform.forward, Common.PlayerBody.transform.up).magnitude;
-            NomaiVR.Log("magnitude", magnitude.ToString());
         }
 
         private void updateMainCamera () {
@@ -81,13 +74,21 @@ namespace NomaiVR {
                 var transform = __instance.GetValue<Transform>("_transform");
 
                 Quaternion fromTo = Quaternion.FromToRotation(transform.forward, Vector3.ProjectOnPlane(playerCam.transform.forward, transform.up));
-                var magnitude = Vector3.ProjectOnPlane(playerCam.transform.forward, transform.up).magnitude;
+
+                var magnitudeUp = 1 - Vector3.ProjectOnPlane(playerCam.transform.up, transform.up).magnitude;
+                var magnitudeForward = 1 - Vector3.ProjectOnPlane(playerCam.transform.up, transform.right).magnitude;
+                var magnitude = magnitudeUp + magnitudeForward;
+
+                if (magnitude < 0.35) {
+                    return;
+                }
 
                 var targetRotation = fromTo * transform.rotation;
                 var inverseRotation = Quaternion.Inverse(fromTo) * playerCam.transform.parent.rotation;
 
-                playerCam.transform.parent.rotation = Quaternion.RotateTowards(playerCam.transform.parent.rotation, inverseRotation, magnitude * magnitude * 2f);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, magnitude * magnitude * 2f);
+                var maxDegreesDelta = magnitude * 5f;
+                playerCam.transform.parent.rotation = Quaternion.RotateTowards(playerCam.transform.parent.rotation, inverseRotation, maxDegreesDelta);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, maxDegreesDelta);
             }
         }
 
