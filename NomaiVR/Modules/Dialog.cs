bbe4eba2b3d9d5e45431d0a2a4e3bf40bafc1400
Ricardo Lopes <raicuparta@gmail.com>
2020-02-16 21:23:31 +0100

    Fixed dialogue canvases

--- NomaiVR/Modules/Dialog.cs
+++ NomaiVR/Modules/Dialog.cs
@@ -5,33 +5,37 @@ namespace NomaiVR {
     public class Dialog: MonoBehaviour {
 
         private static CharacterDialogueTree currentDialogue;
-        private static GameObject dialogCanvas;
+        private static Transform canvasTransform;
         // distance away from the player the dialog renders
         private static float dialogRenderDistance = 1f;
-        private static float dialogRenderSize = 0.001f;
+        private static float dialogRenderSize = 0.0015f;
 
         void Start () {
             NomaiVR.Log("Started Dialog Fixes");
 
             currentDialogue = null;
-            dialogCanvas = GameObject.Find("DialogueCanvas");
-            if (dialogCanvas == null) {
+            canvasTransform = GameObject.Find("DialogueCanvas").transform;
+
+            if (canvasTransform == null) {
                 NomaiVR.Log("Couldn't find canvas with name: DialogueCanvas");
             } else {
-                dialogCanvas.transform.localScale = Vector3.one * dialogRenderSize;
+                canvasTransform.localScale *= dialogRenderSize;
             }
+
+            var canvas = canvasTransform.GetComponentInChildren<Canvas>();
+            canvas.renderMode = RenderMode.WorldSpace;
         }
 
         void Update () {
-            if ((currentDialogue != null) && (dialogCanvas != null)) {
+            if ((currentDialogue != null) && (canvasTransform != null)) {
                 Transform attentionPoint = currentDialogue.GetValue<Transform>("_attentionPoint");
-                dialogCanvas.transform.parent = attentionPoint;
-                dialogCanvas.transform.localPosition = Vector3.zero;
-                dialogCanvas.transform.LookAt(2 * attentionPoint.position - Camera.main.transform.position, Common.PlayerHead.up);
+                canvasTransform.parent = attentionPoint;
+                canvasTransform.localPosition = Vector3.zero;
+                canvasTransform.LookAt(2 * attentionPoint.position - Camera.main.transform.position, Common.PlayerHead.up);
 
                 // Move so it is 1 unit away from the player
                 float distance = Vector3.Distance(attentionPoint.position, Camera.main.transform.position);
-                dialogCanvas.transform.position = Vector3.MoveTowards(attentionPoint.position, Camera.main.transform.position, distance - dialogRenderDistance);
+                canvasTransform.position = Vector3.MoveTowards(attentionPoint.position, Camera.main.transform.position, distance - dialogRenderDistance);
             }
         }
 
