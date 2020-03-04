e23ee11e471c2c588df15aa84cd5ac7aacfb3874
Ricardo Lopes <raicuparta@gmail.com>
2020-03-04 18:29:57 +0100

    Fixed flashlight gesture position

--- NomaiVR/Modules/MotionControls/FlashlightGesture.cs
+++ NomaiVR/Modules/MotionControls/FlashlightGesture.cs
@@ -3,7 +3,7 @@
 namespace NomaiVR {
     class FlashlightGesture: MonoBehaviour {
         void Awake () {
-            var detector = Common.PlayerHead.gameObject.AddComponent<ProximityDetector>();
+            var detector = Locator.GetPlayerCamera().gameObject.AddComponent<ProximityDetector>();
             detector.other = Hands.RightHand;
             detector.localOffset = Vector3.right * 0.15f;
             detector.onEnter += FlashlightPress;
