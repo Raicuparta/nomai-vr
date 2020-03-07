7a5db61afa1dede7c69501c6d8e376b96e332910
Ricardo Lopes <raicuparta@gmail.com>
2020-03-07 23:21:52 +0100

    Move flashlight position and rotation to center of camera

--- NomaiVR/Modules/MotionControls/FlashlightGesture.cs
+++ NomaiVR/Modules/MotionControls/FlashlightGesture.cs
@@ -8,6 +8,14 @@ namespace NomaiVR {
             detector.localOffset = Vector3.right * 0.15f;
             detector.onEnter += FlashlightPress;
             detector.onExit += FlashlightRelease;
+
+            var flashLight = Locator.GetFlashlight().transform;
+            var pivot = flashLight.Find("Flashlight_BasePivot/Flashlight_WobblePivot");
+            var fillLight = pivot.Find("Flashlight_FillLight");
+            var spotLight = pivot.Find("Flashlight_SpotLight");
+
+            fillLight.localPosition = spotLight.localPosition = flashLight.localPosition = Vector3.zero;
+            fillLight.localRotation = spotLight.localRotation = flashLight.localRotation = Quaternion.identity;
         }
 
         void FlashlightPress () {
