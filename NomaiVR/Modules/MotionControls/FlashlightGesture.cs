282d107099889869eb065fd36dfc7d8ad97b03ac
Ricardo Lopes <raicuparta@gmail.com>
2020-03-16 00:52:17 +0100

    Move flashlight a bit
    Closes #166

--- NomaiVR/Modules/MotionControls/FlashlightGesture.cs
+++ NomaiVR/Modules/MotionControls/FlashlightGesture.cs
@@ -9,13 +9,13 @@ namespace NomaiVR {
             detector.onEnter += FlashlightPress;
             detector.onExit += FlashlightRelease;
 
-            var flashLight = Locator.GetFlashlight().transform;
-            var pivot = flashLight.Find("Flashlight_BasePivot/Flashlight_WobblePivot");
+            var flashLight = Locator.GetFlashlight();
+            var pivot = flashLight.transform.Find("Flashlight_BasePivot/Flashlight_WobblePivot");
             var fillLight = pivot.Find("Flashlight_FillLight");
             var spotLight = pivot.Find("Flashlight_SpotLight");
 
-            fillLight.localPosition = spotLight.localPosition = flashLight.localPosition = Vector3.zero;
-            fillLight.localRotation = spotLight.localRotation = flashLight.localRotation = Quaternion.identity;
+            fillLight.localPosition = spotLight.localPosition = flashLight.transform.localPosition = new Vector3(0, 0.05f, 0.05f);
+            fillLight.localRotation = spotLight.localRotation = flashLight.transform.localRotation = Quaternion.identity;
         }
 
         void FlashlightPress () {
