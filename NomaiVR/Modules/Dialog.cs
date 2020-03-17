309e724f9bbcb6cbcc0489d10ab7683f66af265c
Ricardo Lopes <Raicuparta@users.noreply.github.com>
2020-03-17 20:01:19 +0100

    Fix dialogue tokyo-drifting into infinity (#174)
    * Visible button prompt while sleeping
    
    * Make sleep timer canvas visible while eyes closed
    
    * Show eyes animation
    
    * Prevent dialogue from tokyo-drifting into infinity

--- NomaiVR/Modules/Dialog.cs
+++ NomaiVR/Modules/Dialog.cs
@@ -4,37 +4,31 @@ using UnityEngine;
 namespace NomaiVR {
     public class Dialog: MonoBehaviour {
 
-        private static CharacterDialogueTree currentDialogue;
-        private static Transform canvasTransform;
-        // distance away from the player the dialog renders
-        private const float dialogRenderDistance = 1f;
-        private const float dialogRenderSize = 0.0015f;
+        private Transform _canvasTransform;
+        private static Transform _attentionPoint = null;
+        private const float _dialogRenderSize = 0.0015f;
 
         void Start () {
             NomaiVR.Log("Started Dialog Fixes");
 
-            currentDialogue = null;
-            canvasTransform = GameObject.Find("DialogueCanvas").transform;
+            _canvasTransform = GameObject.Find("DialogueCanvas").transform;
 
-            if (canvasTransform == null) {
-                NomaiVR.Log("Couldn't find canvas with name: DialogueCanvas");
-            } else {
-                canvasTransform.localScale *= dialogRenderSize;
-            }
+            _canvasTransform.localScale *= _dialogRenderSize;
+            _canvasTransform.parent = Locator.GetPlayerTransform();
 
-            var canvas = canvasTransform.GetComponentInChildren<Canvas>();
+            var canvas = _canvasTransform.gameObject.GetComponent<Canvas>();
             canvas.renderMode = RenderMode.WorldSpace;
         }
 
         void Update () {
-            if ((currentDialogue != null) && (canvasTransform != null)) {
-                Transform attentionPoint = currentDialogue.GetValue<Transform>("_attentionPoint");
-                canvasTransform.localPosition = Vector3.zero;
-                canvasTransform.LookAt(2 * attentionPoint.position - Camera.main.transform.position, Common.PlayerHead.up);
+            if (_attentionPoint != null && _canvasTransform != null) {
+                var headPosition = Common.PlayerHead.position;
+
+                _canvasTransform.LookAt(2 * _attentionPoint.position - headPosition, Common.PlayerHead.up);
 
                 // Move so it is 1 unit away from the player
-                float distance = Vector3.Distance(attentionPoint.position, Camera.main.transform.position);
-                canvasTransform.position = Vector3.MoveTowards(attentionPoint.position, Camera.main.transform.position, distance - dialogRenderDistance);
+                var offset = (_attentionPoint.position - headPosition).normalized;
+                _canvasTransform.position = headPosition + offset;
             }
         }
 
@@ -44,10 +38,10 @@ namespace NomaiVR {
                 NomaiVR.Pre<CharacterDialogueTree>("EndConversation", typeof(Patches), nameof(Patches.PatchEndConversation));
             }
             static void PatchStartConversation (CharacterDialogueTree __instance) {
-                currentDialogue = __instance;
+                _attentionPoint = __instance.GetValue<Transform>("_attentionPoint");
             }
             static void PatchEndConversation () {
-                currentDialogue = null;
+                _attentionPoint = null;
             }
         }
     }
