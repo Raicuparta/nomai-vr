bd79be37a649bb49dc1afc95b99f3f479f61c450
Ricardo Lopes <raicuparta@gmail.com>
2020-05-06 23:41:43 +0200

    Scroll translator if scrolling needed

--- NomaiVR/Tools/ToolModeInteraction.cs
+++ NomaiVR/Tools/ToolModeInteraction.cs
@@ -1,5 +1,6 @@
 ﻿using OWML.ModHelper.Events;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace NomaiVR {
     class ToolModeInteraction: MonoBehaviour {
@@ -16,8 +17,18 @@ namespace NomaiVR {
                 var translator = FindObjectOfType<NomaiTranslatorProp>();
                 var currentTextId = translator.GetValue<int>("_currentTextID");
                 var nomaiText = translator.GetValue<NomaiText>("_nomaiTextComponent");
+                var textField = translator.GetValue<Text>("_textField");
+                var scrollRect = translator.GetValue<ScrollRect>("_scrollRect");
+                var isScrollable = textField.preferredHeight > scrollRect.viewport.sizeDelta.y;
+                // Using very low value instead of zero, because the final scroll position never gets to exactly zero.
+                var hasMoreToScroll = scrollRect.verticalNormalizedPosition > 0.001f;
 
                 if (!nomaiText) {
+                    return;
+                }
+
+                if (isScrollable && hasMoreToScroll) {
+                    ControllerInput.SimulateInput(XboxAxis.dPadY, -1);
                     return;
                 }
 
@@ -31,6 +42,7 @@ namespace NomaiVR {
 
         void OnDetectorExit () {
             ControllerInput.SimulateInput(XboxAxis.dPadX, 0);
+            ControllerInput.SimulateInput(XboxAxis.dPadY, 0);
         }
     }
 }
