06ea34839b5a40b89736233ca425779a57ad0688
Ricardo Lopes <raicuparta@gmail.com>
2020-05-22 15:04:03 +0200

    Settings class

---
+++ NomaiVR/ModConfig/ModConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace NomaiVR
+{
+    public class ModConfig
+    {
+        public bool showMirrorView = true;
+        public bool debugMode = true;
+        public bool preventCursorLock = true;
+        public bool showHelmet = true;
+        public int overrideRefreshRate = 0;
+
+        public ModConfig()
+        {
+            XRSettings.showDeviceView = showMirrorView;
+
+            if (preventCursorLock)
+            {
+                NomaiVR.Empty<CursorManager>("Update");
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+}
