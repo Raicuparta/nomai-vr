a8037558a8fd42893f83bb3db14ed41d93621754
Ricardo Lopes <Raicuparta@users.noreply.github.com>
2020-06-07 23:31:21 +0200

    Make memory uplink effect come from statue's eyes (#250)
    * Pretty close
    
    * Slightly better
    
    * Clean
    
    * Update lookat

--- NomaiVR/EffectFixes/LoopTransitionFix.cs
+++ NomaiVR/EffectFixes/LoopTransitionFix.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using OWML.ModHelper.Events;
+using UnityEngine;
 
 namespace NomaiVR
 {
@@ -9,17 +10,68 @@ namespace NomaiVR
 
         public class Patch : NomaiVRPatch
         {
+            private static Transform _focus;
+
             public override void ApplyPatches()
             {
-                NomaiVR.Pre<Flashback>("OnTriggerFlashback", typeof(Patch), nameof(Patch.PatchTriggerFlashback));
-                NomaiVR.Pre<Flashback>("Update", typeof(Patch), nameof(Patch.FlashbackUpdate));
+                NomaiVR.Pre<Flashback>("OnTriggerFlashback", typeof(Patch), nameof(PatchTriggerFlashback));
+                NomaiVR.Pre<Flashback>("Update", typeof(Patch), nameof(FlashbackUpdate));
+                NomaiVR.Pre<Flashback>("UpdateMemoryUplink", typeof(Patch), nameof(PostUpdateMemoryLink));
+                NomaiVR.Post<Flashback>("OnTriggerMemoryUplink", typeof(Patch), nameof(PostTriggerMemoryUplink));
 
                 // Prevent flashing on energy death.
                 NomaiVR.Post<Flashback>("OnTriggerFlashback", typeof(Patch), nameof(PostTriggerFlashback));
             }
 
+            private static void PostUpdateMemoryLink(
+                GameObject ____reverseStreams,
+                Transform ____screenTransform,
+                ref Vector3 ____origScreenScale,
+                ref float ____reverseScreenEndDist,
+                ref float ____reverseScreenStartDist
+            )
+            {
+                if (_focus != null)
+                {
+                    _focus.LookAt(Camera.main.transform, Locator.GetPlayerTransform().up);
+                }
+            }
+
+            private static void PostTriggerMemoryUplink(
+                GameObject ____reverseStreams,
+                Transform ____screenTransform,
+                ref Vector3 ____origScreenScale,
+                ref float ____reverseScreenEndDist,
+                ref float ____reverseScreenStartDist
+            )
+            {
+                ____reverseScreenEndDist = 0.2f;
+                ____reverseScreenStartDist = 2.5f;
+                ____origScreenScale *= 0.5f;
+                var scale = ____origScreenScale;
+
+                var uplinkTrigger = GameObject.FindObjectOfType<MemoryUplinkTrigger>();
+                var statue = uplinkTrigger.GetValue<Transform>("_lockOnTransform");
+                var eye = statue.Find("Props_NOM_StatueHead/eyelid_mid");
+                _focus = new GameObject().transform;
+                _focus.SetParent(eye, false);
+
+                var streams = ____reverseStreams.transform;
+                LayerHelper.ChangeLayerRecursive(____reverseStreams, LayerMask.NameToLayer("UI"));
+                streams.SetParent(_focus, false);
+                streams.Rotate(0, 180, 0);
+                streams.localScale *= 0.5f;
+
+                var screen = ____screenTransform;
+                LayerHelper.ChangeLayerRecursive(screen.gameObject, LayerMask.NameToLayer("UI"));
+                screen.SetParent(_focus, false);
+                screen.localRotation = Quaternion.identity;
+                screen.localScale = scale;
+            }
+
             private static void PatchTriggerFlashback(Flashback __instance, Transform ____maskTransform, Transform ____screenTransform)
             {
+                NomaiVR.Log("Trigger flashback");
                 Transform parent;
 
                 if (____screenTransform.parent == __instance.transform)
