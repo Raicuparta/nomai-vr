e1b73795319f06e764d50c68ce3dd762f138f056
Ricardo Lopes <raicuparta@gmail.com>
2020-07-05 17:58:05 +0200

    Remove yellow

--- NomaiVR/Helpers/TextHelper.cs
+++ NomaiVR/Helpers/TextHelper.cs
@@ -5,7 +5,6 @@
         public const string RED = "#ff8484";
         public const string GREEN = "#c2ff81";
         public const string BLUE = "#8ed3ff";
-        public const string YELLOW = "#fffd69";
         public const string ORANGE = "orange";
 
         public static string TextWithColor(string text, string color)
