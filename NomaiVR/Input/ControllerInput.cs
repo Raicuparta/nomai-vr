75ff358f7bfc239e34a39bc96ade35cab136bede
Ricardo Lopes <raicuparta@gmail.com>
2020-07-28 13:59:28 +0200

    Rename frequency to seconds from now

--- NomaiVR/Input/ControllerInput.cs
+++ NomaiVR/Input/ControllerInput.cs
@@ -457,13 +457,13 @@ namespace NomaiVR
                         a.y *= 1.42857146f;
 
                         var haptic = SteamVR_Actions.default_Haptic;
-                        var frequency = 0.1f;
+                        var secondsFromNow = 0.1f;
                         var amplitudeY = a.y * NomaiVR.Config.vibrationStrength;
                         var amplitudeX = a.x * NomaiVR.Config.vibrationStrength;
-                        haptic.Execute(0, frequency, 10, amplitudeY, SteamVR_Input_Sources.RightHand);
-                        haptic.Execute(0, frequency, 50, amplitudeX, SteamVR_Input_Sources.RightHand);
-                        haptic.Execute(0, frequency, 10, amplitudeY, SteamVR_Input_Sources.LeftHand);
-                        haptic.Execute(0, frequency, 50, amplitudeX, SteamVR_Input_Sources.LeftHand);
+                        haptic.Execute(0, secondsFromNow, 10, amplitudeY, SteamVR_Input_Sources.RightHand);
+                        haptic.Execute(0, secondsFromNow, 50, amplitudeX, SteamVR_Input_Sources.RightHand);
+                        haptic.Execute(0, secondsFromNow, 10, amplitudeY, SteamVR_Input_Sources.LeftHand);
+                        haptic.Execute(0, secondsFromNow, 50, amplitudeX, SteamVR_Input_Sources.LeftHand);
                     }
 
                     return false;
