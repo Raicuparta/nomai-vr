2b86143d5652eeaab1d404eb2295435056bb825d
Ricardo Lopes <raicuparta@gmail.com>
2020-08-02 12:07:49 +0200

    Visible controller model

--- NomaiVR/UI/ControllerModel.cs
+++ NomaiVR/UI/ControllerModel.cs
@@ -1,30 +1,50 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 namespace NomaiVR
 {
     internal class ControllerModel : NomaiVRModule<ControllerModel.Behaviour, ControllerModel.Behaviour.Patch>
     {
         protected override bool IsPersistent => false;
-        protected override OWScene[] Scenes => PlayableScenes;
+        protected override OWScene[] Scenes => AllScenes;
 
         public class Behaviour : MonoBehaviour
         {
             private static List<ScreenPromptList> _worldPromptLists;
+            private SteamVR_RenderModel _renderModel;
+
+            internal void Start()
+            {
+                SetUpModel();
+                ShowModel();
+            }
 
             internal void Update()
             {
-                UpdateControllerModelVisibility();
+                //UpdateControllerModelVisibility();
+            }
+
+            private void SetUpModel()
+            {
+                _renderModel = new GameObject("SteamVR_RenderModel").AddComponent<SteamVR_RenderModel>();
+                _renderModel.transform.parent = HandsController.Behaviour.LeftHand;
+                _renderModel.transform.localPosition = Vector3.zero;
+                _renderModel.transform.localRotation = Quaternion.identity;
+                _renderModel.transform.localScale = Vector3.one;
+
+                _renderModel.SetInputSource(SteamVR_Input_Sources.LeftHand);
+                _renderModel.SetDeviceIndex(1);
             }
 
             private void ShowModel()
             {
-
+                _renderModel.gameObject.SetActive(true);
             }
 
             private void HideModel()
             {
-
+                _renderModel.gameObject.SetActive(false);
             }
 
             private void UpdateControllerModelVisibility()
