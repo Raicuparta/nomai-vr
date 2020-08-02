26e6480d4b98e672c708524d7031a491b21b7c22
Ricardo Lopes <Raicuparta@users.noreply.github.com>
2020-08-02 15:14:39 +0200

    Slightly smoother HUD rotation (#327)

---
+++ NomaiVR/ReusableBehaviours/SmoothCamereRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace NomaiVR
+{
+    public class SmoothCamereRotation : MonoBehaviour
+    {
+        private Quaternion _lastFrameRotation;
+        private float _speed = 1f;
+
+        internal void Start()
+        {
+            _lastFrameRotation = transform.rotation;
+        }
+
+        internal void LateUpdate()
+        {
+            var targetRotation = Camera.main.transform.rotation;
+            var difference = Mathf.Abs(Quaternion.Angle(_lastFrameRotation, targetRotation));
+
+            var step = _speed * Time.unscaledDeltaTime * difference * difference;
+            transform.rotation = Quaternion.RotateTowards(_lastFrameRotation, targetRotation, step);
+            _lastFrameRotation = transform.rotation;
+        }
+    }
+}
