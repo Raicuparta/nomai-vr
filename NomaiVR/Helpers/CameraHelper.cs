b903783f339e91e39cda656f5b861462c9340819
Ricardo Lopes <raicuparta@gmail.com>
2020-08-13 18:39:43 +0200

    Fix camera helper missing camera error

--- NomaiVR/Helpers/CameraHelper.cs
+++ NomaiVR/Helpers/CameraHelper.cs
@@ -6,6 +6,11 @@ namespace NomaiVR
     {
         public static bool IsOnScreen(Vector3 position)
         {
+            var camera = Camera.current;
+            if (!camera)
+            {
+                return false;
+            }
             var screenPoint = Camera.current.WorldToViewportPoint(position);
             return screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
         }
