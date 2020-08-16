0e6df48a8a8c210a39e46e3487ad38922dc7a7b3
Ricardo Lopes <raicuparta@gmail.com>
2020-08-16 22:31:45 +0200

    Fix regression with IsUsingAnyTool

--- NomaiVR/Helpers/ToolHelper.cs
+++ NomaiVR/Helpers/ToolHelper.cs
@@ -4,7 +4,17 @@
     {
         public static ToolModeSwapper Swapper => Locator.GetToolModeSwapper();
 
-        public static bool IsUsingAnyTool(ToolGroup group = ToolGroup.None)
+        public static bool IsUsingAnyTool()
+        {
+            if (Swapper == null)
+            {
+                return false;
+            }
+
+            return Swapper.IsInToolMode(ToolMode.Probe) || Swapper.IsInToolMode(ToolMode.Translator) || Swapper.IsInToolMode(ToolMode.SignalScope);
+        }
+
+        public static bool IsUsingAnyTool(ToolGroup group)
         {
             if (Swapper == null)
             {
