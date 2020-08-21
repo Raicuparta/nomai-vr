1e4f3b4334bd8aec0dc89a521c1516387d1edcc2
Ricardo Lopes <raicuparta@gmail.com>
2020-08-21 23:49:11 +0200

    Cleanup Hand.cs

--- NomaiVR/Hands/Hand.cs
+++ NomaiVR/Hands/Hand.cs
@@ -12,23 +12,28 @@ namespace NomaiVR
 
         internal void Start()
         {
-            var hand = Instantiate(handPrefab).transform;
-            var glove = Instantiate(glovePrefab).transform;
-            hand.gameObject.AddComponent<ConditionalRenderer>().getShouldRender += ShouldRenderHands;
-            hand.GetComponentInChildren<Renderer>().material.shader = Shader.Find("Outer Wilds/Character/Skin");
-            glove.gameObject.AddComponent<ConditionalRenderer>().getShouldRender += ShouldRenderGloves;
-            glove.GetComponentInChildren<Renderer>().material.shader = Shader.Find("Outer Wilds/Character/Clothes");
-
-            SetUpHandModel(hand);
-            SetUpHandModel(glove);
-
-            gameObject.SetActive(false);
-            var poseDriver = transform.gameObject.AddComponent<SteamVR_Behaviour_Pose>();
-            poseDriver.poseAction = pose;
-            gameObject.SetActive(true);
+            SetUpHandModel();
+            SetUpGloveModel();
+            SetUpVrPose();
         }
 
-        void SetUpHandModel(Transform model)
+        private void SetUpHandModel()
+        {
+            var hand = Instantiate(handPrefab).transform;
+            hand.gameObject.AddComponent<ConditionalRenderer>().getShouldRender += ShouldRenderHands;
+            hand.GetComponentInChildren<Renderer>().material.shader = Shader.Find("Outer Wilds/Character/Skin");
+            SetUpModel(hand);
+        }
+
+        private void SetUpGloveModel()
+        {
+            var glove = Instantiate(glovePrefab).transform;
+            glove.gameObject.AddComponent<ConditionalRenderer>().getShouldRender += ShouldRenderGloves;
+            glove.GetComponentInChildren<Renderer>().material.shader = Shader.Find("Outer Wilds/Character/Clothes");
+            SetUpModel(glove);
+        }
+
+        private void SetUpModel(Transform model)
         {
             model.parent = transform;
             model.localPosition = transform.localPosition;
@@ -38,6 +43,14 @@ namespace NomaiVR
             {
                 model.localScale = new Vector3(-model.localScale.x, model.localScale.y, model.localScale.z);
             }
+        }
+
+        private void SetUpVrPose()
+        {
+            gameObject.SetActive(false);
+            var poseDriver = transform.gameObject.AddComponent<SteamVR_Behaviour_Pose>();
+            poseDriver.poseAction = pose;
+            gameObject.SetActive(true);
         }
 
         private bool ShouldRenderGloves()
