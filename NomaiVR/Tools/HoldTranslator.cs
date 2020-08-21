bd227c6a7304370eff8a725f4343617747edc234
Ricardo Lopes <raicuparta@gmail.com>
2020-08-21 23:29:47 +0200

    Cleanup HoldTranslator

--- NomaiVR/Tools/HoldTranslator.cs
+++ NomaiVR/Tools/HoldTranslator.cs
@@ -11,28 +11,46 @@ namespace NomaiVR
 
         public class Behaviour : MonoBehaviour
         {
-            private LineRenderer _lineRenderer;
-
             internal void Start()
             {
-                var translator = Camera.main.transform.Find("NomaiTranslatorProp");
+                var translator = SetUpTranslator();
+                SetUpHoldable(translator);
+                var translatorGroup = SetUpTranslatorGroup(translator);
+                var translatorModel = SetUpTranslatorModel(translatorGroup);
+                SetUpLaser(translator);
+                RemoveTextMaterials(translator);
+                SetUpHolster(translatorModel);
+                SetUpLaser(translator);
+            }
 
+            private Transform SetUpTranslator()
+            {
+                var translator = Locator.GetPlayerCamera().transform.Find("NomaiTranslatorProp");
+                translator.localScale = Vector3.one * 0.3f;
+                return translator;
+            }
+
+            private void SetUpHoldable(Transform translator)
+            {
                 var holdTranslator = translator.gameObject.AddComponent<Holdable>();
                 holdTranslator.transform.localPosition = new Vector3(-0.2f, 0.107f, 0.02f);
                 holdTranslator.transform.localRotation = Quaternion.Euler(32.8f, 0f, 0f);
+            }
 
+            private Transform SetUpTranslatorGroup(Transform translator)
+            {
                 var translatorGroup = translator.Find("TranslatorGroup");
                 translatorGroup.localPosition = Vector3.zero;
                 translatorGroup.localRotation = Quaternion.identity;
+                translatorGroup.Find("TranslatorBeams").localScale = Vector3.one / 0.3f;
+                return translatorGroup;
+            }
 
-                translator.localScale = Vector3.one * 0.3f;
+            private Transform SetUpTranslatorModel(Transform translatorGroup)
+            {
                 var translatorModel = translatorGroup.Find("Props_HEA_Translator");
                 translatorModel.localPosition = Vector3.zero;
                 translatorModel.localRotation = Quaternion.identity;
-
-                var laser = SetUpLaser(translator);
-
-                translator.GetComponent<NomaiTranslator>().SetValue("_raycastTransform", laser);
 
                 // This child seems to be only for some kind of shader effect.
                 // Disabling it since it looks glitchy and doesn't seem necessary.
@@ -48,38 +66,20 @@ namespace NomaiVR
                     }
                 }
 
-                var texts = translator.gameObject.GetComponentsInChildren<Graphic>(true);
-
-                foreach (var text in texts)
-                {
-                    text.material = null;
-                }
-
-                var translatorHolster = Instantiate(translatorModel).gameObject;
-                translatorHolster.SetActive(true);
-                var holster = translatorHolster.AddComponent<HolsterTool>();
-                holster.hand = HandsController.Behaviour.RightHand;
-                holster.position = new Vector3(-0.3f, 0, 0);
-                holster.mode = ToolMode.Translator;
-                holster.scale = 0.15f;
-                holster.angle = new Vector3(0, 90, 90);
-
-                translatorGroup.Find("TranslatorBeams").localScale = Vector3.one / 0.3f;
-
-                SetUpLaser(translator);
+                return translatorModel;
             }
 
             private Transform SetUpLaser(Transform translator)
             {
                 var lineObject = new GameObject("Translator Line");
-                _lineRenderer = lineObject.AddComponent<LineRenderer>();
-                _lineRenderer.useWorldSpace = false;
-                _lineRenderer.SetPositions(new[] { Vector3.zero, Vector3.forward * 6f });
-                _lineRenderer.startWidth = 0.005f;
-                _lineRenderer.endWidth = 0.001f;
-                _lineRenderer.endColor = Color.clear;
-                _lineRenderer.startColor = new Color(0.4f, 0.5f, 0.8f, 0.3f); ;
-                _lineRenderer.material.shader = Shader.Find("Particles/Alpha Blended Premultiply");
+                var lineRenderer = lineObject.AddComponent<LineRenderer>();
+                lineRenderer.useWorldSpace = false;
+                lineRenderer.SetPositions(new[] { Vector3.zero, Vector3.forward * 6f });
+                lineRenderer.startWidth = 0.005f;
+                lineRenderer.endWidth = 0.001f;
+                lineRenderer.endColor = Color.clear;
+                lineRenderer.startColor = new Color(0.4f, 0.5f, 0.8f, 0.3f); ;
+                lineRenderer.material.shader = Shader.Find("Particles/Alpha Blended Premultiply");
 
                 lineObject.transform.SetParent(translator, false);
                 lineObject.transform.localPosition = new Vector3(0.74f, 0.37f, 0f);
@@ -87,8 +87,30 @@ namespace NomaiVR
 
                 lineObject.AddComponent<ConditionalRenderer>().getShouldRender = () => ToolHelper.Swapper.IsInToolMode(ToolMode.Translator, ToolGroup.Suit);
 
-                return lineObject.transform;
+                translator.GetComponent<NomaiTranslator>().SetValue("_raycastTransform", lineObject.transform);
 
+                return lineObject.transform;
+            }
+
+            private void RemoveTextMaterials(Transform translator)
+            {
+                var texts = translator.gameObject.GetComponentsInChildren<Graphic>(true);
+                foreach (var text in texts)
+                {
+                    text.material = null;
+                }
+            }
+
+            private void SetUpHolster(Transform translatorModel)
+            {
+                var holsterModel = Instantiate(translatorModel).gameObject;
+                holsterModel.SetActive(true);
+                var holsterTool = holsterModel.AddComponent<HolsterTool>();
+                holsterTool.hand = HandsController.Behaviour.RightHand;
+                holsterTool.position = new Vector3(-0.3f, 0, 0);
+                holsterTool.mode = ToolMode.Translator;
+                holsterTool.scale = 0.15f;
+                holsterTool.angle = new Vector3(0, 90, 90);
             }
 
             public class Patch : NomaiVRPatch
