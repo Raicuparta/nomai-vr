493f7f7370b379e7a9a1b61289fc33c036ed0b18
Jacopo Libè <jacopo.libe@gmail.com>
2021-09-30 21:44:35 +0200

    Shadow fix fix

--- NomaiVR/EffectFixes/ShadowsFix.cs
+++ NomaiVR/EffectFixes/ShadowsFix.cs
@@ -28,21 +28,29 @@ namespace NomaiVR
                 Prefix<LightLOD>(nameof(LightLOD.RevertLODSettings), nameof(Patch.OWCameraPostRenderDisabler));
                 Prefix<RingworldSunController>(nameof(RingworldSunController.OnOWCameraPostRender), nameof(Patch.OWCameraPostRenderDisabler));
                 Prefix<IPExteriorVisualsManager>(nameof(IPExteriorVisualsManager.OnOWCameraPostRender), nameof(Patch.OWCameraPostRenderDisabler));
-                Prefix<TessellatedSphereRenderer>(nameof(TessellatedSphereRenderer.Clear), nameof(Patch.OWCameraPostRenderDisabler));
-                Prefix<TessellatedPlaneRenderer>(nameof(TessellatedPlaneRenderer.Clear), nameof(Patch.OWCameraPostRenderDisabler));
-                Prefix<TessellatedRingRenderer>(nameof(TessellatedRingRenderer.Clear), nameof(Patch.OWCameraPostRenderDisabler));
+                //Prefix<TessellatedSphereRenderer>(nameof(TessellatedSphereRenderer.Clear), nameof(Patch.OWCameraPostRenderDisablerLeft));
+                //Prefix<TessellatedPlaneRenderer>(nameof(TessellatedPlaneRenderer.Clear), nameof(Patch.OWCameraPostRenderDisablerLeft));
+                //Prefix<TessellatedRingRenderer>(nameof(TessellatedRingRenderer.Clear), nameof(Patch.OWCameraPostRenderDisablerLeft));
+                //Prefix<TessellatedSphereRenderer>(nameof(TessellatedSphereRenderer.Rebuild), nameof(Patch.OWCameraPostRenderDisablerLeft));
+                //Prefix<TessellatedPlaneRenderer>(nameof(TessellatedPlaneRenderer.Rebuild), nameof(Patch.OWCameraPostRenderDisablerLeft));
+                //Prefix<TessellatedRingRenderer>(nameof(TessellatedRingRenderer.Rebuild), nameof(Patch.OWCameraPostRenderDisablerLeft));
 
-                //Ensure Light Event order
-                //FIXME: even after this, lighting is still wrong
-                Prefix<ProxyShadowLight>(nameof(ProxyShadowLight.OnEnable), nameof(Patch.Pre_ProxyShadowLight_Enable));
-                Prefix<ProxyShadowLight>(nameof(ProxyShadowLight.OnDisable), nameof(Patch.Pre_ProxyShadowLight_Disable));
-                Prefix<CubeLight>(nameof(CubeLight.AddCommandBuffers), nameof(Patch.Pre_CubeLight_AddCommandBuffers));
-                Prefix<CubeLight>(nameof(CubeLight.RemoveCommandBuffers), nameof(Patch.Pre_CubeLight_RemoveCommandBuffers));
+                //Ensure CommandBuffers are only built once
+                Prefix<ProxyShadowLight>(nameof(ProxyShadowLight.BuildCommandBuffers), nameof(Patch.Pre_ProxyShadowLight_BuildCommandBuffers));
+                Prefix<ProxyShadowLight>(nameof(ProxyShadowLight.ClearCommandBuffers), nameof(Patch.Pre_ProxyShadowLight_ClearCommandBuffers));
+
+                Prefix<UnderwaterCurrentFadeController>(nameof(UnderwaterCurrentFadeController.OnAnyPrerender), nameof(Patch.Pre_UnderwaterCurrentFadeController_OnAnyPrerender));
+                Prefix<UnderwaterCurrentFadeController>(nameof(UnderwaterCurrentFadeController.OnAnyPostrender), nameof(Patch.Pre_UnderwaterCurrentFadeController_OnAnyPostRender));
+            }
+
+            private static bool OWCameraPostRenderDisablerLeft(OWCamera owCamera)
+            {
+                return owCamera.mainCamera == null || !owCamera.mainCamera.stereoEnabled || owCamera.mainCamera.stereoActiveEye != Camera.MonoOrStereoscopicEye.Right;
             }
 
             private static bool OWCameraPostRenderDisabler(OWCamera owCamera)
             {
-                return !owCamera.mainCamera.stereoEnabled || owCamera.mainCamera.stereoActiveEye != Camera.MonoOrStereoscopicEye.Left;
+                return owCamera.mainCamera == null || !owCamera.mainCamera.stereoEnabled || owCamera.mainCamera.stereoActiveEye != Camera.MonoOrStereoscopicEye.Left;
             }
 
             private static bool PreOnAnyCameraPostRender(Camera camera)
@@ -50,53 +58,26 @@ namespace NomaiVR
                 return !camera.stereoEnabled || camera.stereoActiveEye != Camera.MonoOrStereoscopicEye.Left;
             }
 
-            private static bool Pre_ProxyShadowLight_Enable(ProxyShadowLight __instance)
+            //ProxyShadowLight updates for some reason have garbage data on the left eye, only update them on the right
+            private static bool Pre_ProxyShadowLight_BuildCommandBuffers(Camera camera)
             {
-                __instance._light.AddCommandBuffer(LightEvent.BeforeScreenspaceMask, __instance._renderCommandBuffer);
-                __instance._light.AddCommandBuffer(LightEvent.AfterShadowMap, __instance._paramsCommandBuffer);
-                Camera.onPreRender = (Camera.CameraCallback)Delegate.Combine(Camera.onPreRender, new Camera.CameraCallback(__instance.BuildCommandBuffers));
-                Camera.onPostRender = (Camera.CameraCallback)Delegate.Combine(Camera.onPostRender, new Camera.CameraCallback(__instance.ClearCommandBuffers));
-                return false;
+                return !camera.stereoEnabled || camera.stereoActiveEye != Camera.MonoOrStereoscopicEye.Left;
             }
 
-            private static bool Pre_ProxyShadowLight_Disable(ProxyShadowLight __instance)
+            private static bool Pre_ProxyShadowLight_ClearCommandBuffers(Camera camera)
             {
-                __instance._light.RemoveCommandBuffer(LightEvent.BeforeScreenspaceMask, __instance._renderCommandBuffer);
-                __instance._light.RemoveCommandBuffer(LightEvent.AfterShadowMap, __instance._paramsCommandBuffer);
-                Camera.onPreRender = (Camera.CameraCallback)Delegate.Remove(Camera.onPreRender, new Camera.CameraCallback(__instance.BuildCommandBuffers));
-                Camera.onPostRender = (Camera.CameraCallback)Delegate.Remove(Camera.onPostRender, new Camera.CameraCallback(__instance.ClearCommandBuffers));
-                return false;
+                return !camera.stereoEnabled || camera.stereoActiveEye != Camera.MonoOrStereoscopicEye.Right;
             }
 
-            //FIXME: The order here is probably scuffed, I didn't see a single light of this kind for now... probably DLC content
-            private static bool Pre_CubeLight_AddCommandBuffers(CubeLight __instance)
+            //ProxyShadowLight updates for some reason have garbage data on the left eye, only update them on the right
+            private static bool Pre_UnderwaterCurrentFadeController_OnAnyPrerender(OWCamera owCamera)
             {
-                __instance._light.AddCommandBuffer(LightEvent.AfterScreenspaceMask, __instance._clear2DTexCommandBuffer);
-                for (int i = 0; i < 6; i++)
-                {
-                    //These can probably run in parallel
-                    __instance._light.AddCommandBuffer(LightEvent.BeforeScreenspaceMask, __instance._faceCommandBuffers[i]);
-                }
-                __instance._light.AddCommandBuffer(LightEvent.AfterShadowMap, __instance._paramsCommandBuffer);
-                return false;
+                return owCamera.mainCamera == null || !owCamera.mainCamera.stereoEnabled || owCamera.mainCamera.stereoActiveEye != Camera.MonoOrStereoscopicEye.Right;
             }
 
-            private static bool Pre_CubeLight_RemoveCommandBuffers(CubeLight __instance)
+            private static bool Pre_UnderwaterCurrentFadeController_OnAnyPostRender(OWCamera owCamera)
             {
-                if (__instance._faceCommandBuffers == null)
-                {
-                    return false;
-                }
-                __instance._light.RemoveCommandBuffer(LightEvent.AfterScreenspaceMask, __instance._clear2DTexCommandBuffer);
-                for (int i = 0; i < 6; i++)
-                {
-                    if (__instance._faceCommandBuffers[i] != null)
-                    {
-                        __instance._light.RemoveCommandBuffer(LightEvent.BeforeScreenspaceMask, __instance._faceCommandBuffers[i]);
-                    }
-                }
-                __instance._light.RemoveCommandBuffer(LightEvent.AfterShadowMap, __instance._paramsCommandBuffer);
-                return false;
+                return owCamera.mainCamera == null || !owCamera.mainCamera.stereoEnabled || owCamera.mainCamera.stereoActiveEye != Camera.MonoOrStereoscopicEye.Left;
             }
         }
     }
