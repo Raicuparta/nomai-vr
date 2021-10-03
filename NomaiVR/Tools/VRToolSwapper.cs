8e5d155fac98063e0281f4cf7622eb95b47beebf
Jacopo Libè <jacopo.libe@gmail.com>
2021-10-03 03:30:44 +0200

    Switched back to old stable VRToolSwapper

--- NomaiVR/Tools/VRToolSwapper.cs
+++ NomaiVR/Tools/VRToolSwapper.cs
@@ -9,13 +9,12 @@ namespace NomaiVR
         protected override OWScene[] Scenes => PlayableScenes;
 
         private static bool _isBuccklingUp = false;
-        private static Hand _pendingHandInteract;
         public static Hand InteractingHand { get; private set; }
         public static Hand NonInteractingHand { get; private set; }
 
         public static event Action InteractingHandChanged;
-        public static event Action Equipped;
         public static event Action ToolEquipped;
+        public static event Action Equipped;
         public static event Action UnEquipped;
 
         private static readonly Dictionary<ToolMode, bool> _toolsAllowedToEquip = new Dictionary<ToolMode, bool>() {
@@ -25,16 +24,18 @@ namespace NomaiVR
         public static void Equip(ToolMode mode, Hand interactingHand)
         {
             _toolsAllowedToEquip[mode] = true;
-            _pendingHandInteract = interactingHand;
             ToolHelper.Swapper.EquipToolMode(mode);
+            UpdateHand(interactingHand);
+            Equipped?.Invoke();
+            if (mode != ToolMode.Item && mode != ToolMode.None) ToolEquipped?.Invoke();
             _toolsAllowedToEquip[mode] = false;
         }
 
         public static void Unequip()
         {
-            _toolsAllowedToEquip[ToolMode.None] = true;
-            ToolHelper.Swapper.UnequipTool();
-            _toolsAllowedToEquip[ToolMode.None] = false;
+            UpdateHand(null);
+            UnEquipped?.Invoke();
+            Equip(ToolMode.None, null);
         }
 
         private static void UpdateHand(Hand interactingHand)
@@ -53,24 +54,22 @@ namespace NomaiVR
             InteractingHandChanged?.Invoke();
         }
 
+        public static bool IsAllowedToEquip(ToolMode mode)
+        {
+            if (_isBuccklingUp || (OWInput.IsInputMode(InputMode.ShipCockpit) && mode == ToolMode.None))
+            {
+                _isBuccklingUp = false;
+                return true;
+            }
+            return _toolsAllowedToEquip.ContainsKey(mode) && _toolsAllowedToEquip[mode];
+        }
+
         public class Patch : NomaiVRPatch
         {
             public override void ApplyPatches()
             {
-                Prefix<ToolModeSwapper>(nameof(ToolModeSwapper.EquipToolMode), nameof(PreEquipTool));
-                Postfix<ToolModeSwapper>(nameof(ToolModeSwapper.EquipToolMode), nameof(PostEquipTool));
-                Prefix<ToolModeSwapper>(nameof(ToolModeSwapper.UnequipTool), nameof(PreUnequipTool));
-                Prefix<ShipCockpitController>(nameof(ShipCockpitController.OnPressInteract), nameof(PreShipCockpitController));
-            }
-
-            public static bool IsAllowedToEquip(ToolMode mode)
-            {
-                if (_isBuccklingUp || (OWInput.IsInputMode(InputMode.ShipCockpit) && mode == ToolMode.None))
-                {
-                    _isBuccklingUp = false;
-                    return true;
-                }
-                return _toolsAllowedToEquip.ContainsKey(mode) && _toolsAllowedToEquip[mode];
+                Prefix<ToolModeSwapper>("EquipToolMode", nameof(PreEquipTool));
+                Prefix<ShipCockpitController>("OnPressInteract", nameof(PreShipCockpitController));
             }
 
             private static void PreShipCockpitController()
@@ -78,31 +77,9 @@ namespace NomaiVR
                 _isBuccklingUp = true;
             }
 
-            private static bool PreEquipTool(ToolMode mode, ToolModeSwapper __instance, out PlayerTool __state)
+            private static bool PreEquipTool(ToolMode mode)
             {
-                __state = __instance._equippedTool;
                 return IsAllowedToEquip(mode);
-            }
-
-            private static void PostEquipTool(ToolModeSwapper __instance, PlayerTool __state)
-            {
-                if(__state != __instance._equippedTool
-                    && __instance._equippedTool != null)
-                {
-                    UpdateHand(_pendingHandInteract);
-                    _pendingHandInteract = null;
-                    Equipped?.Invoke();
-                    if (__instance._currentToolMode != ToolMode.Item) ToolEquipped?.Invoke();
-                }
-            }
-
-            private static void PreUnequipTool()
-            {
-                if (IsAllowedToEquip(ToolMode.None))
-                {
-                    UpdateHand(null);
-                    UnEquipped?.Invoke();
-                }
             }
         }
     }
