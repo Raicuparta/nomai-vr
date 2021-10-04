8a28aef23475629b60b937e433731bc87b81b14e
Raicuparta <raicuparta@gmail.com>
2021-10-04 05:33:14 +0200

    new VRActionInputs

--- NomaiVR/Input/InputMap.cs
+++ NomaiVR/Input/InputMap.cs
@@ -1,147 +1,44 @@
 ﻿using System.Collections.Generic;
-using Valve.VR;
 using static InputConsts;
-using static Valve.VR.SteamVR_Actions;
 
 namespace NomaiVR.Input
 {
     public static class InputMap
     {
-        public class VRActionInput
+        public static readonly Dictionary<InputCommandType, VRActionInputs.VRActionInput> Map = new Dictionary<InputCommandType, VRActionInputs.VRActionInput>()
         {
-            public ISteamVR_Action Action;
-        }
-        
-        public static readonly Dictionary<InputCommandType, VRActionInput> Map = new Dictionary<InputCommandType, VRActionInput>()
-        {
-            {
-                InputCommandType.JUMP, 
-                new VRActionInput() { Action = _default.Jump }
-            },
-            {
-                InputCommandType.BOOST, 
-                new VRActionInput() { Action = _default.Jump }
-            },
-            {
-                InputCommandType.SWAP_SHIP_LOG_MODE,
-                new VRActionInput() { Action = _default.Jump }
-            },
-            {
-                InputCommandType.INTERACT_SECONDARY,
-                new VRActionInput() { Action = _default.Jump }
-            },
-            {
-                InputCommandType.MATCH_VELOCITY,
-                new VRActionInput() { Action = _default.Jump }
-            },
-            {
-                InputCommandType.UP,
-                new VRActionInput() { Action = _default.UIDpad }
-            },
-            {
-                InputCommandType.DOWN,
-                new VRActionInput() { Action = _default.UIDpad }
-            },
-            {
-                InputCommandType.RIGHT,
-                new VRActionInput() { Action = _default.UIDpad }
-            },
-            {
-                InputCommandType.MENU_RIGHT,
-                new VRActionInput() { Action = _default.UIDpad }
-            },
-            {
-                InputCommandType.LEFT,
-                new VRActionInput() { Action = _default.UIDpad }
-            },
-            {
-                InputCommandType.MENU_LEFT,
-                new VRActionInput() { Action = _default.UIDpad }
-            },
-            {
-                InputCommandType.TABR,
-                new VRActionInput() { Action = _default.UITabRight }
-            },
-            {
-                InputCommandType.TABL,
-                new VRActionInput() { Action = _default.UITabLeft }
-            },
-            {
-                InputCommandType.SUBMENU_RIGHT,
-                new VRActionInput() { Action = _default.UISubtabRight }
-            },
-            {
-                InputCommandType.SUBMENU_LEFT,
-                new VRActionInput() { Action = _default.UISubtabLeft }
-            },
-            {
-                InputCommandType.MAP,
-                new VRActionInput() { Action = _default.Map }
-            },
-            {
-                InputCommandType.PAUSE,
-                new VRActionInput() { Action = _default.Menu }
-            },
-            {
-                InputCommandType.CONFIRM,
-                new VRActionInput() { Action = _default.Menu }
-            },
-            {
-                InputCommandType.ESCAPE,
-                new VRActionInput() { Action = _default.Back }
-            },
-            {
-                InputCommandType.CANCEL,
-                new VRActionInput() { Action = _default.Back }
-            },
-            {
-                InputCommandType.SELECT,
-                new VRActionInput() { Action = _default.Interact }
-            },
-            {
-                InputCommandType.INTERACT,
-                new VRActionInput() { Action = _default.Interact }
-            },
-            {
-                InputCommandType.LOCKON,
-                new VRActionInput() { Action = _default.Interact }
-            },
-            {
-                InputCommandType.ENTER,
-                new VRActionInput() { Action = _default.Interact }
-            },
-            {
-                InputCommandType.LOOK,
-                new VRActionInput() { Action = _default.Look }
-            },
-            {
-                InputCommandType.LOOK_X,
-                new VRActionInput() { Action = _default.Look }
-            },
-            {
-                InputCommandType.LOOK_Y,
-                new VRActionInput() { Action = _default.Look }
-            },
-            {
-                InputCommandType.MOVE_XZ,
-                new VRActionInput() { Action = _default.Move }
-            },
-            {
-                InputCommandType.MOVE_X,
-                new VRActionInput() { Action = _default.Move }
-            },
-            {
-                InputCommandType.MOVE_Z,
-                new VRActionInput() { Action = _default.Move }
-            },
-            {
-                InputCommandType.THRUST_UP,
-                new VRActionInput() { Action = _default.ThrustUp }
-            },
-            {
-                InputCommandType.THRUST_DOWN,
-                new VRActionInput() { Action = _default.ThrustDown }
-            },
+            { InputCommandType.JUMP, VRActionInputs.Jump },
+            { InputCommandType.BOOST, VRActionInputs.Jump },
+            { InputCommandType.SWAP_SHIP_LOG_MODE, VRActionInputs.Jump },
+            { InputCommandType.INTERACT_SECONDARY, VRActionInputs.Jump },
+            { InputCommandType.MATCH_VELOCITY, VRActionInputs.Jump },
+            { InputCommandType.UP, VRActionInputs.UIDpad },
+            { InputCommandType.DOWN, VRActionInputs.UIDpad },
+            { InputCommandType.RIGHT, VRActionInputs.UIDpad },
+            { InputCommandType.MENU_RIGHT, VRActionInputs.UIDpad },
+            { InputCommandType.LEFT, VRActionInputs.UIDpad },
+            { InputCommandType.MENU_LEFT, VRActionInputs.UIDpad },
+            { InputCommandType.TABR, VRActionInputs.UITabRight },
+            { InputCommandType.TABL, VRActionInputs.UITabLeft },
+            { InputCommandType.SUBMENU_RIGHT, VRActionInputs.UISubtabRight },
+            { InputCommandType.SUBMENU_LEFT, VRActionInputs.UISubtabLeft },
+            { InputCommandType.MAP, VRActionInputs.Map },
+            { InputCommandType.PAUSE, VRActionInputs.Menu },
+            { InputCommandType.CONFIRM, VRActionInputs.Menu },
+            { InputCommandType.ESCAPE, VRActionInputs.Back },
+            { InputCommandType.CANCEL, VRActionInputs.Back },
+            { InputCommandType.SELECT, VRActionInputs.Interact },
+            { InputCommandType.INTERACT, VRActionInputs.Interact },
+            { InputCommandType.LOCKON, VRActionInputs.Interact },
+            { InputCommandType.ENTER, VRActionInputs.Interact },
+            { InputCommandType.LOOK, VRActionInputs.Look },
+            { InputCommandType.LOOK_X, VRActionInputs.Look },
+            { InputCommandType.LOOK_Y, VRActionInputs.Look },
+            { InputCommandType.MOVE_XZ, VRActionInputs.Move },
+            { InputCommandType.MOVE_X, VRActionInputs.Move },
+            { InputCommandType.MOVE_Z, VRActionInputs.Move },
+            { InputCommandType.THRUST_UP, VRActionInputs.ThrustUp },
+            { InputCommandType.THRUST_DOWN, VRActionInputs.ThrustDown },
         };
     }
 }
---
+++ NomaiVR/Input/VRActionInputs.cs
@@ -0,0 +1,35 @@
+﻿using Valve.VR;
+using static Valve.VR.SteamVR_Actions;
+
+namespace NomaiVR.Input
+{
+    public static class VRActionInputs
+    {
+        public class VRActionInput
+        {
+            public ISteamVR_Action Action;
+        }
+
+        public static readonly VRActionInput Autopilot = new VRActionInput() {Action = _default.Autopilot};
+        public static readonly VRActionInput Back = new VRActionInput() {Action = _default.Back};
+        public static readonly VRActionInput Grip = new VRActionInput() {Action = _default.Grip};
+        public static readonly VRActionInput Interact = new VRActionInput() {Action = _default.Interact};
+        public static readonly VRActionInput Jump = new VRActionInput() {Action = _default.Jump};
+        public static readonly VRActionInput Look = new VRActionInput() {Action = _default.Look};
+        public static readonly VRActionInput Map = new VRActionInput() {Action = _default.Map};
+        public static readonly VRActionInput Menu = new VRActionInput() {Action = _default.Menu};
+        public static readonly VRActionInput Move = new VRActionInput() {Action = _default.Move};
+        public static readonly VRActionInput Recenter = new VRActionInput() {Action = _default.Recenter};
+        public static readonly VRActionInput RollMode = new VRActionInput() {Action = _default.RollMode};
+        public static readonly VRActionInput StationaryDpad = new VRActionInput() {Action = _default.StationaryDpad};
+        public static readonly VRActionInput StationaryUse = new VRActionInput() {Action = _default.StationaryUse};
+        public static readonly VRActionInput ThrustDown = new VRActionInput() {Action = _default.ThrustDown};
+        public static readonly VRActionInput ThrustUp = new VRActionInput() {Action = _default.ThrustUp};
+        public static readonly VRActionInput UIDpad = new VRActionInput() {Action = _default.UIDpad};
+        public static readonly VRActionInput UISelect = new VRActionInput() {Action = _default.UISelect};
+        public static readonly VRActionInput UISubtabLeft = new VRActionInput() {Action = _default.UISubtabLeft};
+        public static readonly VRActionInput UISubtabRight = new VRActionInput() {Action = _default.UISubtabRight};
+        public static readonly VRActionInput UITabLeft = new VRActionInput() {Action = _default.UITabLeft};
+        public static readonly VRActionInput UITabRight = new VRActionInput() {Action = _default.UITabRight};
+    }
+}
