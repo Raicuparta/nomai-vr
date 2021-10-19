550c56c318281830953a2bb90450c04fde0c4624
Raicuparta <raicuparta@gmail.com>
2021-10-19 22:50:02 +0200

    Adjusted autopilot button color and text

--- NomaiVR/Tools/AutopilotButtonPatch.cs
+++ NomaiVR/Tools/AutopilotButtonPatch.cs
@@ -1,8 +1,6 @@
-﻿using NomaiVR.Input;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace NomaiVR.Tools
 {
@@ -13,26 +11,38 @@ namespace NomaiVR.Tools
 
         public class AutopilotButton : MonoBehaviour
         {
+            private enum AutopilotButtonState
+            {
+                PreInit,
+                Off,
+                Available,
+                Active,
+            }
+
+            private static readonly int _pressAnimation = Animator.StringToHash("Press");
+            private static readonly int _emissionColor = Shader.PropertyToID("_EmissionColor");
+            private static readonly Color _onColor = new Color(0.19f,0.34f,0.63f);
+            private static readonly Color _offColor =  Color.black;
+            private static readonly Color _activeColor = new Color(0.99f,0.65f,0.44f);
+            private Color _textDefaultColor;
             private Animator _animator;
+            private Text _text;
             private Material _buttonTopMat;
-            private Color _onColor;
             private ShipMonitorInteraction _interactor;
             private ShipCockpitController _cockpitController;
-            private bool _autoPilotLightEnabled = false;
+            private AutopilotButtonState _buttonState = AutopilotButtonState.PreInit;
 
             private void Awake()
             {
                 var cockpitTech = FindObjectOfType<ShipBody>().transform.Find("Module_Cockpit/Geo_Cockpit/Cockpit_Tech/Cockpit_Tech_Interior");
                 _cockpitController = FindObjectOfType<ShipCockpitController>();
-
-
+                
                 //Add autopilot button
-                var autopilotButton = GameObject.Instantiate(AssetLoader.AutopilotButtonPrefab);
-                autopilotButton.transform.SetParent(transform, false);
-
+                var autopilotButton = Instantiate(AssetLoader.AutopilotButtonPrefab, transform, false);
                 _animator = autopilotButton.GetComponentInChildren<Animator>();
+                _text = autopilotButton.GetComponentInChildren<Text>();
+                _textDefaultColor = _text.color;
                 _buttonTopMat = autopilotButton.transform.Find("AutoPilot_ButtonTop").GetComponent<Renderer>().material;
-                _onColor = _buttonTopMat.GetColor("_EmissionColor");
 
                 transform.SetParent(cockpitTech, false);
                 transform.localPosition = cockpitTech.Find("LandingCamScreen").transform.localPosition + new Vector3(-0.93f, 3.6f, 5.2f);
@@ -42,47 +52,81 @@ namespace NomaiVR.Tools
                 _interactor.button = InputConsts.InputCommandType.AUTOPILOT;
                 _interactor.skipPressCallback = OnPress;
 
-                _buttonTopMat.SetColor("_EmissionColor", Color.black);
-            }
-
-            private bool OnPress()
-            {
-                _animator.SetTrigger("Press");
-                return false;
+                SetState(AutopilotButtonState.Off);
             }
 
             private void Update()
             {
-                if (_cockpitController.IsAutopilotAvailable() && !_autoPilotLightEnabled)
-                {
-                    _autoPilotLightEnabled = true;
-                    _buttonTopMat.SetColor("_EmissionColor", _onColor);
-                }
-
-                if (!_cockpitController.IsAutopilotAvailable() && _autoPilotLightEnabled)
-                {
-                    _autoPilotLightEnabled = false;
-                    _buttonTopMat.SetColor("_EmissionColor", Color.black);
-                }
-
+                UpdateState();
                 UpdatePrompts();
             }
 
+            private void UpdateState()
+            {
+                if (IsAutopilotActive())
+                {
+                    SetState(AutopilotButtonState.Active);
+                }
+                else if (_cockpitController.IsAutopilotAvailable())
+                {
+                    SetState(AutopilotButtonState.Available);
+                }
+                else if (!_cockpitController.IsAutopilotAvailable())
+                {
+                    SetState(AutopilotButtonState.Off);
+                }
+            }
+            
             private void UpdatePrompts()
             {
-                if (_cockpitController._autopilot.IsFlyingToDestination()
-                    && _interactor.text != UITextType.ShipAbortAutopilotPrompt)
+                if (IsAutopilotActive() && _interactor.text != UITextType.ShipAbortAutopilotPrompt)
                 {
                     _interactor.text = UITextType.ShipAbortAutopilotPrompt;
                     _interactor.receiver?.SetPromptText(_interactor.text);
                 }
 
-                if (!_cockpitController._autopilot.IsFlyingToDestination()
-                    && _interactor.text != UITextType.ShipAutopilotPrompt)
+                if (!IsAutopilotActive() && _interactor.text != UITextType.ShipAutopilotPrompt)
                 {
                     _interactor.text = UITextType.ShipAutopilotPrompt;
                     _interactor.receiver?.SetPromptText(_interactor.text);
                 }
+            }
+
+            private bool IsAutopilotActive()
+            {
+                return _cockpitController && _cockpitController._autopilot.IsFlyingToDestination();
+            }
+
+            private void SetState(AutopilotButtonState state)
+            {
+                if (state == _buttonState) return;
+                switch (state)
+                {
+                    case AutopilotButtonState.Active:
+                        SetButtonColor(_activeColor);
+                        _text.color = Color.black;
+                        break;
+                    case AutopilotButtonState.Available:
+                        SetButtonColor(_onColor);
+                        _text.color = Color.white;
+                        break;
+                    default:
+                        SetButtonColor(_offColor);
+                        _text.color = _textDefaultColor;
+                        break;
+                }
+                _buttonState = state;
+            }
+
+            private void SetButtonColor(Color color)
+            {
+                _buttonTopMat.SetColor(_emissionColor, color);
+            }
+
+            private bool OnPress()
+            {
+                _animator.SetTrigger(_pressAnimation);
+                return false;
             }
         }
     }
