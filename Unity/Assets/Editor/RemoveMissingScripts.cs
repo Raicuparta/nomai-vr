b0dd4fd1c525dc9c7727e7264c5b8e9fbd5386c7
Raicuparta <raicuparta@gmail.com>
2021-10-28 21:17:47 +0200

    Add ship cockpit model

---
+++ Unity/Assets/Editor/RemoveMissingScripts.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public static class RemoveMissingScripts {
+        [MenuItem("Tools/Remove missing scripts from selected object")]
+        private static void RecursiveCleanupMissingScripts()
+        {
+            var allTransforms = Selection.gameObjects[0].GetComponentsInChildren<Transform>(true);
+            foreach (var transform in allTransforms)
+            {
+                var gameObject = transform.gameObject;
+
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(gameObject);
+            }
+        }
+    }
+}
