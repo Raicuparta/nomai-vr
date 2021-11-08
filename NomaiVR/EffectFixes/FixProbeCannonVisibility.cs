fa03d000e8f26e7ccd4e2103894c5d40a3a612ca
Raicuparta <raicuparta@gmail.com>
2021-11-08 07:01:14 +0100

    Rotate TH and player

---
+++ NomaiVR/EffectFixes/FixProbeCannonVisibility.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace NomaiVR.EffectFixes
+{
+    internal class FixProbeCannonVisibility: NomaiVRModule<NomaiVRModule.EmptyBehaviour, FixProbeCannonVisibility.Patch>
+    {
+        protected override bool IsPersistent => false;
+        protected override OWScene[] Scenes => SolarSystemScene;
+
+        internal class Patch: NomaiVRPatch
+        {
+            public override void ApplyPatches()
+            {
+                Prefix<AstroObject>(nameof(AstroObject.Awake), nameof(RotateTimberHearth));
+                Postfix<PlayerSpawner>(nameof(PlayerSpawner.SpawnPlayer), nameof(RotatePlayer));
+            }
+            
+            private static void RotateTimberHearth(AstroObject __instance)
+            {
+                if (__instance._name != AstroObject.Name.TimberHearth) return;
+
+                __instance.transform.eulerAngles += new Vector3(37, 18, 0);
+            }
+
+            private static void RotatePlayer(PlayerSpawner __instance)
+            {
+                if (__instance._initialSpawnPoint == null || LoadManager.GetCurrentScene() != OWScene.SolarSystem) return;
+                var playerTransform = __instance._playerBody.transform;
+		        var vector2 = - playerTransform.position;
+		        var vector3 = vector2 - Vector3.Project(vector2, playerTransform.up);
+		        var lhs = Quaternion.AngleAxis(Vector3.Angle(playerTransform.forward, vector3) * 45, playerTransform.up);
+		        playerTransform.rotation = lhs * playerTransform.rotation;
+                
+            }
+        }
+    }
+}
