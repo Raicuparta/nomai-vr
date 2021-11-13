2a12fe2f4d5311a5c4ee8aca15afd9784b5ea792
Raicuparta <raicuparta@gmail.com>
2021-11-13 19:21:30 +0100

    Fix rigidbodies not rotating with TH

--- NomaiVR/EffectFixes/FixProbeCannonVisibility.cs
+++ NomaiVR/EffectFixes/FixProbeCannonVisibility.cs
@@ -1,49 +1,57 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using NomaiVR.Helpers;
+using UnityEngine;
 
 namespace NomaiVR.EffectFixes
 {
     // Seeing the Probe Cannon explosion when waking up from each loop is a pretty important detail in the game.
     // In VR, the player no longer wakes up facing the sky, so this detail is much easier to miss.
     // By rotating TH, we can make GD show at an angle that's visible while looking straight ahead.
-    internal class FixProbeCannonVisibility: NomaiVRModule<NomaiVRModule.EmptyBehaviour, FixProbeCannonVisibility.Patch>
+    internal class FixProbeCannonVisibility: NomaiVRModule<FixProbeCannonVisibility.Behaviour, FixProbeCannonVisibility.Behaviour.Patch>
     {
         protected override bool IsPersistent => false;
         protected override OWScene[] Scenes => SolarSystemScene;
 
-        internal class Patch: NomaiVRPatch
+        internal class Behaviour : MonoBehaviour
         {
-            public override void ApplyPatches()
+            internal class Patch: NomaiVRPatch
             {
-                Prefix<AstroObject>(nameof(AstroObject.Awake), nameof(RotateTimberHearth));
-                Postfix<PlayerSpawner>(nameof(PlayerSpawner.SpawnPlayer), nameof(RotatePlayer));
-            }
+                private static bool isInitialized;
             
-            private static void RotateTimberHearth(AstroObject __instance)
-            {
-                if (__instance._name != AstroObject.Name.TimberHearth) return;
+                public override void ApplyPatches()
+                {
+                    Prefix<OWRigidbody>(nameof(OWRigidbody.Awake), nameof(RotateTimberHearth));
+                    Postfix<PlayerSpawner>(nameof(PlayerSpawner.SpawnPlayer), nameof(RotatePlayer));
+                    
+                    LoadManager.OnStartSceneLoad += (scene, loadScene) => isInitialized = false;
+                }
+                
+                private static void RotateTimberHearth()
+                {
+                    if (isInitialized || !SceneHelper.IsInSolarSystem()) return;
+                    isInitialized = true;
+                    var timberHearth = FindObjectsOfType<AstroObject>().First(astroObject => astroObject.GetAstroObjectName() == AstroObject.Name.TimberHearth);
+                    if (!timberHearth) return;
+                    timberHearth.transform.eulerAngles += new Vector3(37, 18, 0);
+                }
 
-                var shipTransform = GameObject.FindWithTag("Ship").transform;
-                var shipParent = shipTransform.parent;
-                shipTransform.SetParent(__instance.transform);
-                __instance.transform.eulerAngles += new Vector3(37, 18, 0);
-                shipTransform.SetParent(shipParent);
-            }
-
-            private static void RotatePlayer(PlayerSpawner __instance)
-            {
-                if (__instance._initialSpawnPoint == null || LoadManager.GetCurrentScene() != OWScene.SolarSystem) return;
-                var playerTransform = __instance._playerBody.transform;
-		        var playerPosition = playerTransform.position;
-                var playerUp = playerTransform.up;
-                var playerForward = playerTransform.forward;
-		        var projectedPosition = Vector3.Project(playerPosition, playerUp) - playerPosition;
-                var giantsDeepPosition = Locator.GetAstroObject(AstroObject.Name.GiantsDeep).transform.position;
-                var projectedForward = Vector3.Project(playerForward, playerUp);
-                var siantsDeepDirection = Vector3.Project(giantsDeepPosition - playerPosition, playerUp);
-                var angle = Vector3.Angle(projectedForward, siantsDeepDirection);
-		        var rotationOffset = Quaternion.AngleAxis(Vector3.Angle(playerForward, projectedPosition) + angle, playerUp);
-		        playerTransform.rotation = rotationOffset * playerTransform.rotation;
+                private static void RotatePlayer(PlayerSpawner __instance)
+                {
+                    if (__instance._initialSpawnPoint == null || LoadManager.GetCurrentScene() != OWScene.SolarSystem) return;
+                    var playerTransform = __instance._playerBody.transform;
+		            var playerPosition = playerTransform.position;
+                    var playerUp = playerTransform.up;
+                    var playerForward = playerTransform.forward;
+		            var projectedPosition = Vector3.Project(playerPosition, playerUp) - playerPosition;
+                    var giantsDeepPosition = Locator.GetAstroObject(AstroObject.Name.GiantsDeep).transform.position;
+                    var projectedForward = Vector3.Project(playerForward, playerUp);
+                    var siantsDeepDirection = Vector3.Project(giantsDeepPosition - playerPosition, playerUp);
+                    var angle = Vector3.Angle(projectedForward, siantsDeepDirection);
+		            var rotationOffset = Quaternion.AngleAxis(Vector3.Angle(playerForward, projectedPosition) + angle, playerUp);
+		            playerTransform.rotation = rotationOffset * playerTransform.rotation;
+                }
             }
         }
+        
     }
 }
